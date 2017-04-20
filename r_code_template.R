18b0ae7271db4bb2474e5095fa21ae59c26be443
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-04-20 16:35:40 -0700

    A template used for R code programs

---
+++ r_code_template.R
@@ -0,0 +1,21 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# Description: 
+
+# source and library statements
+
+# function definitions
+HereIsASampleFunction <- function(x,y,something=TRUE) {
+  # quick description
+  #
+  # Args:
+  #   x: what x is
+  #   y: what y is
+  #   something: if TRUE then something
+  #
+  # Returns:
+  #   What comes back from the function
+  
+}
+
+# program body
