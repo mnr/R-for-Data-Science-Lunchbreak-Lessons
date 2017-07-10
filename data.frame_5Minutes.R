67c4d7691782522c683788c75d5fbb3183a39a9b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-07-10 14:25:12 -0700

    just getting started

---
+++ data.frame_5Minutes.R
@@ -0,0 +1,7 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# Description: Example file for data frame: five minutes of R
+
+# data frame is like a spreadsheet
+# multidimensional. Columns can be different types
+# unlike a list, all rows must be equal number of observations
