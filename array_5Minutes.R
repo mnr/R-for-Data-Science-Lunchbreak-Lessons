8346702d6a1ed29f3b3cf861681e2e95a9d0ee61
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-07-10 14:09:56 -0700

    a bit more knowledge and a grammar fix

--- array_5Minutes.R
+++ array_5Minutes.R
@@ -11,5 +11,22 @@ I.am.a.longer.vector <- c(I.am.a.vector,c("did","gyre","and","gimble","in","wabe
 
 # array(data_vector,dim_vector)
 # dimension is expressed as row, column, z-index
-I.am.a.array <- array(I.am.a.longer.vector,c(2,3,2))
-I.am.a.array
+I.am.an.array <- array(I.am.a.longer.vector,c(2,3,2))
+I.am.an.array
+
+# indexing into the array
+I.am.an.array[2,3,2]
+toupper(I.am.an.array[2,3,2])
+I.am.an.array[2,3,2] <- toupper(I.am.an.array[2,3,2])
+I.am.an.array[2,,2] # omission returns the entire range. Here - all columns of row 2, table 2
+
+# this fails because there is not a third level
+I.am.an.array[2,3,3]
+
+# dimnames
+my.row.names <- c("up","down")
+my.column.names <- c("left","middle","right")
+my.table.names <- c("behind","in front")
+dimnames(I.am.an.array) <- list(my.row.names,my.column.names,my.table.names)
+I.am.an.array
+I.am.an.array["down",3,2]
