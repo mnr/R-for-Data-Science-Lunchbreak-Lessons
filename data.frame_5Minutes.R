2097322056d5b59da632d3cd67a289e73b3718c7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-07-12 21:45:03 -0600

    additions & changes

--- data.frame_5Minutes.R
+++ data.frame_5Minutes.R
@@ -3,7 +3,7 @@
 # Description: Example file for data frame: five minutes of R
 
 # data frame is like a spreadsheet
-# Columns are vectors
+# Columns (aka: variables) are vectors
 # Rows (aka: observations) are lists and must contain an equal number of columns
 
 I.am.a.vector <- c(1,2,3,4,5,6)
@@ -13,4 +13,28 @@ many.months <- month.abb[1:6]
 I.am.a.dataframe <- data.frame(I.am.a.vector,I.am.also.a.vector,many.months)
 
 I.am.a.short.vector <- c(1:5)
-I.am.a.dataframe <- data.frame(I.am.a.vector,I.am.a.short.vector) #oops - different # of rows
+I.am.a.failure <- data.frame(I.am.a.vector,I.am.a.short.vector) #oops - different # of rows
+
+I.am.a.dataframe[2,3] # col 3 is a factor (used to be a string)
+
+str(I.am.a.dataframe) # structure of the dataframe
+nrow(I.am.a.dataframe) # Number of Rows
+ncol(I.am.a.dataframe) # Number of Columns
+head(I.am.a.dataframe) # first rows of dataframe
+tail(I.am.a.dataframe) # last rows of dataframe
+
+
+# row.names ---------------------------------------------------------------
+I.am.a.dataframe <- data.frame(I.am.a.vector,many.months,row.names = I.am.also.a.vector)
+I.am.a.dataframe["slithey","many.months"]
+row.names(I.am.a.dataframe)
+
+# stringsAsFactors --------------------------------------------------------
+# I() is "Inhibit conversion)
+str(I.am.a.dataframe)
+I.am.a.dataframe <- data.frame(I.am.a.vector,I(many.months),row.names = I.am.also.a.vector)
+str(I.am.a.dataframe)
+I.am.a.dataframe["slithey","many.months"]
+
+# Next, we'll discuss using cbind and rbind to add rows and columns
+
