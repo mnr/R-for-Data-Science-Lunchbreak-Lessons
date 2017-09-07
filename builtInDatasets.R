8ad6101dfae3791c0a135f273913dfe2e349c124
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-09-06 17:26:24 -0700

    Update and rename builtInDatasets_5Minutes.R to builtInDatasets.R

--- builtInDatasets.R
+++ builtInDatasets.R
@@ -1,6 +1,9 @@
 # Copyright Mark Niemann-Ross, 2017
 # Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# Description: Example file for datasets: five minutes of R
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: Example file for datasets
 
 # documentation of datasets
 library(help="datasets")
