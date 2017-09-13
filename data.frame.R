fe70d2afa3555c8a429fd33890a7977b6b0482c2
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-09-13 04:07:06 -0700

    Update and rename data.frame_5Minutes.R to data.frame.R

--- data.frame.R
+++ data.frame.R
@@ -1,6 +1,9 @@
 # Copyright Mark Niemann-Ross, 2017
 # Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# Description: Example file for data frame: five minutes of R
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: Example file for data frame
 
 # data frame is like a spreadsheet
 # Columns (aka: variables) are vectors
@@ -12,10 +15,9 @@ many.months <- month.abb[1:6]
 
 I.am.a.dataframe <- data.frame(I.am.a.vector,I.am.also.a.vector,many.months)
 
+# vectors need to have identical lengths (lengths aka "observations")
 I.am.a.short.vector <- c(1:5)
 I.am.a.failure <- data.frame(I.am.a.vector,I.am.a.short.vector) #oops - different # of rows
-
-I.am.a.dataframe[2,3] # col 3 is a factor (used to be a string)
 
 str(I.am.a.dataframe) # structure of the dataframe
 nrow(I.am.a.dataframe) # Number of Rows (observations)
@@ -24,16 +26,18 @@ head(I.am.a.dataframe) # first rows of dataframe
 tail(I.am.a.dataframe) # last rows of dataframe
 names(I.am.a.dataframe) # Header names (aka variable names)
 
-# row.names ---------------------------------------------------------------
-I.am.a.dataframe <- data.frame(I.am.a.vector,many.months,row.names = I.am.also.a.vector)
-I.am.a.dataframe["slithey","many.months"]
-row.names(I.am.a.dataframe)
-
 # stringsAsFactors --------------------------------------------------------
-# I() is "Inhibit conversion)
+# strings are converted to factors - this is a default behavior
+I.am.a.dataframe[2,3] # col 3 is a factor (used to be a string)
+# I() is "Inhibit conversion
 str(I.am.a.dataframe)
 I.am.a.dataframe <- data.frame(I.am.a.vector,I(many.months),row.names = I.am.also.a.vector)
 str(I.am.a.dataframe)
 I.am.a.dataframe["slithey","many.months"]
 
+# row.names ---------------------------------------------------------------
+I.am.a.dataframe <- data.frame(I.am.a.vector,many.months,row.names = I.am.also.a.vector)
+I.am.a.dataframe["slithey","many.months"]
+row.names(I.am.a.dataframe)
 
+# check out tibbles - https://cran.r-project.org/web/packages/tibble/vignettes/tibble.html
