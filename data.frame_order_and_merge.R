b3c91a728e4780d4d66e03fad5965207758c5184
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-09-21 15:35:46 -0700

    Break out cbind and rbind as a separate session

--- data.frame_order_and_merge.R
+++ data.frame_order_and_merge.R
@@ -3,19 +3,19 @@
 # LinkedIn: https://www.linkedin.com/in/markniemannross/
 # Github: https://github.com/mnr
 # More Learning: http://niemannross.com/link/mnratlil
-# Description: Example file for Dataframes: order, merge, rbind, and cbind
+# Description: Example file for Dataframes: order and merge
 
 # Need a dataframe to play with
 data("ChickWeight") # a built-in dataset
 
-# data.frames:order -------------------------------------------------------
+# data.frames: order -------------------------------------------------------
 ChickWeight$weight # produces unsorted list of "weight" values
 sort(ChickWeight$weight) # sorts a vector
 order(ChickWeight$weight) # What is this? answer: sorted row numbers - not values
 ChickWeight[196,] #lists the observation with the smallest weight
 ChickWeight[order(ChickWeight$weight),] # comma because [ row - comma - column ]
 
-# data.frames:merge -------------------------------------------------------
+# data.frames: merge -------------------------------------------------------
 # first, create two data.frames
 chick.one <- ChickWeight[ChickWeight$Chick == 1,]
 chick.two <- ChickWeight[ChickWeight$Chick == 2,]
@@ -28,10 +28,3 @@ match.time.obs <- merge(chick.one,chick.two,by="Time")
 source("chicknames.R") # builds a data.frame with ranked names
 chicks_names <- merge(ChickWeight,chicknames) # oops! merge is confused, so does an sql join all
 chicks_names <- merge(ChickWeight,chicknames,by.x="Chick",by.y = "rank")
-
-# data.frames:rbind -------------------------------------------------------
-# row bind. Combine columns by rows
-
-# data.frames:cbind --------------------------------------------------------
-# column bind. Combine dataframes by columns
-
