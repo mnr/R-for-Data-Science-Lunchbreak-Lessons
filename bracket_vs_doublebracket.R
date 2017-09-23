19dd81fc7216600ddb58affd47ca73f00e58d353
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-09-23 03:35:01 -0700

    setting up example

---
+++ bracket_vs_doublebracket.R
@@ -0,0 +1,10 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: Example file for [] vs [[]]
+
+ChickWeight["Chick"]
+ChickWeight["Chick"][3] # fails
+ChickWeight[["Chick"]][3] # succeeds
