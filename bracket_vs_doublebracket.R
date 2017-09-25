fa4c22d64ac610584a288434a1a16db0d487b2df
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-09-25 16:43:15 -0700

    added examples

--- bracket_vs_doublebracket.R
+++ bracket_vs_doublebracket.R
@@ -6,5 +6,25 @@
 # Description: Example file for [] vs [[]]
 
 ChickWeight["Chick"]
-ChickWeight["Chick"][3] # fails
-ChickWeight[["Chick"]][3] # succeeds
+ChickWeight["Chick"][3] # fails. returns object
+ChickWeight[["Chick"]][3] # succeeds. returns contents of object
+
+# another example
+lotsaletters <- list(CAPS=LETTERS[1:10], lc=letters[1:10])
+class(lotsaletters) # list of 2 objects
+lotsaletters #just to take a look
+
+# CAPS is an element of lotsaletters
+lotsaletters[1] # $CAPS (an object)
+lotsaletters$CAPS # lists contents of CAPS
+lotsaletters[["CAPS"]] # identical to $CAPS
+
+# structure of each
+class(lotsaletters[1]) # CAPS object
+class(lotsaletters$CAPS) #contents of CAPS object
+class(lotsaletters[["CAPS"]]) # identical to str(lotsaletters$CAPS)
+
+# contents of each
+lotsaletters[1][3] # NULL because [1] returns an object - not the contents of an object
+lotsaletters$CAPS[3] # "C" because $CAPS returns contents of CAPS object
+lotsaletters[["CAPS"]][3] # "C" because [[]] returns contents
