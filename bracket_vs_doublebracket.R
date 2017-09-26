c747a8be3d7e0053ce76675250b05281a7052bf2
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-09-26 15:32:36 -0700

    added overview and better explanation

--- bracket_vs_doublebracket.R
+++ bracket_vs_doublebracket.R
@@ -11,7 +11,7 @@ ChickWeight[["Chick"]][3] # succeeds. returns contents of object
 
 # another example
 lotsaletters <- list(CAPS=LETTERS[1:10], lc=letters[1:10])
-class(lotsaletters) # list of 2 objects
+typeof(lotsaletters) # list of 2 objects
 lotsaletters #just to take a look
 
 # CAPS is an element of lotsaletters
@@ -20,9 +20,9 @@ lotsaletters$CAPS # lists contents of CAPS
 lotsaletters[["CAPS"]] # identical to $CAPS
 
 # structure of each
-class(lotsaletters[1]) # CAPS object
-class(lotsaletters$CAPS) #contents of CAPS object
-class(lotsaletters[["CAPS"]]) # identical to str(lotsaletters$CAPS)
+typeof(lotsaletters[1]) # CAPS object
+typeof(lotsaletters$CAPS) #contents of CAPS object
+typeof(lotsaletters[["CAPS"]]) # identical to str(lotsaletters$CAPS)
 
 # contents of each
 lotsaletters[1][3] # NULL because [1] returns an object - not the contents of an object
