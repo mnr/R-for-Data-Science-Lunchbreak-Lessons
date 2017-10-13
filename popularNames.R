fc306779f2bc883603af51db7811c56632831349
Mark Niemann-Ross <mark.niemannross@gmail.com>
2017-10-13 12:33:59 -0700

    change to match online course

--- popularNames.R
+++ popularNames.R
@@ -1,16 +1,67 @@
 # creates a data.frame with ranked names
 # used by data.frame merge example
 popularNames <-
-structure(list(rank = 1:50, name = c("Noah", "Emma", "Liam", 
-"Isabella", "Mason", "Mia", "Jacob", "Abigail", "William", "Elizabeth", 
-"Ethan", "Avery", "James", "Chloe", "Alexander", "Harper", "Michael", 
-"Aubrey", "Benjamin", "Evelyn", "Elijah", "Grace", "Daniel", 
-"Zoey", "Aiden", "Lillian", "Logan", "Brooklyn", "Matthew", 
-"Lucas", "Zoe", "Jackson", "Audrey", "David", "Anna", "Oliver", 
-"Aaliyah", "Joseph", "Camila", "Gabriel", "Kaylee", "Samuel", 
-"Hailey", "Carter", "Riley", "Anthony", "Nevaeh", "John", "Claire", 
-"Dylan")), .Names = c("rank", "name"), row.names = c(NA, 50L), class = "data.frame")
+  structure(
+    list(
+      rank = 1:50,
+      name = c(
+        "Sophia",
+        "Emma",
+        "Olivia",
+        "Isabella",
+        "Ava",
+        "Mia",
+        "Emily",
+        "Abigail",
+        "Madison",
+        "Elizabeth",
+        "Charlotte",
+        "Avery",
+        "Sofia",
+        "Chloe",
+        "Sammy",
+        "Harper",
+        "Bryce",
+        "Aubrey",
+        "Addison",
+        "Evelyn",
+        "Andrew",
+        "Stephen",
+        "Hannah",
+        "Zoey",
+        "Daniel",
+        "Lillian",
+        "Dennis",
+        "Brooklyn",
+        "Samantha",
+        "Todd",
+        "Zoe",
+        "Michael",
+        "Audrey",
+        "Allison",
+        "Anna",
+        "Savannah",
+        "Aaliyah",
+        "Gabriella",
+        "Camila",
+        "Aria",
+        "Kaylee",
+        "Scarlett",
+        "Hailey",
+        "Arianna",
+        "Riley",
+        "Peter",
+        "Nevaeh",
+        "Sarah",
+        "Hulk",
+        "Sadie"
+      )
+    ),
+    .Names = c("rank", "name"),
+    row.names = c(NA, 50L),
+    class = "data.frame"
+  )
 
 data("ChickWeight") # a built-in dataset
-chick.one <- ChickWeight[ChickWeight$Chick == 1,]
-chick.two <- ChickWeight[ChickWeight$Chick == 2,]
+chick.one <- ChickWeight[ChickWeight$Chick == 1, ]
+chick.two <- ChickWeight[ChickWeight$Chick == 2, ]
