48a99611e0cac9a8811e20e090f16e972509f60b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-01-26 13:45:07 -0800

    initial commit

---
+++ 01_26_style_guides.R
@@ -0,0 +1,21 @@
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: R language style guides
+
+# main idea: Writing readable R code
+
+# google's stye guide: https://google.github.io/styleguide/Rguide.xml
+# Hadley Wickham's style guide: http://adv-r.had.co.nz/Style.html
+# Tidyverse style guide: http://style.tidyverse.org/
+# Aroma coding style: http://aroma-project.org/developers/
+# Paul Johnson: https://cran.r-project.org/web/packages/rockchalk/vignettes/Rstyle.pdf
+
+install.packages("lintr")
+
+install.packages("formatR")
+library(formatR)
+tidy_source()
+
