9abc772afae76891bd032ffd32dd5a4e04f4f5b1
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-02-01 10:43:51 -0800

    broke out rbind from c(rbind + cbind)

--- 01_13_data.frame_rbind.R
+++ 01_13_data.frame_rbind.R
@@ -3,9 +3,9 @@
 # LinkedIn: https://www.linkedin.com/in/markniemannross/
 # Github: https://github.com/mnr
 # More Learning: http://niemannross.com/link/mnratlil
-# Description: Example file for Dataframes: rbind and cbind
+# Description: Example file for Dataframes: rbind
 
-# Main Idea: how to add rows and columns to data.frames (or matrices)
+# Main Idea: how to add rows to data.frames (or matrices)
 
 # Need some data to play with
 source("makeChickWeight.R") # builds a data.frame with ranked names
@@ -24,10 +24,4 @@ chicks.one.and.two <- rbind(chick.one.renamed,chick.two) # error: names don't ma
 # rbind matches by column name - not column position
 chick.one.reordered <- chick.one[,c(3,1,2,4)]
 chicks.one.and.two <- rbind(chick.one.reordered,chick.two) # result column order is reordered
-
-# data.frames: cbind --------------------------------------------------------
-# column bind. Combine dataframes (or matrices or vectors) by columns
-# cbind, unlike merge, does not attempt to match values between columns 
-cbind_chicks_names <- cbind(ChickWeight,chicknames) # oops. # of Rows must be equal
-cbind_chicks_names <- cbind(ChickWeight[1:50,],chicknames)
 
