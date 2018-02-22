b0ddb013cadb844d7cd6eba9ea56eeb8c96fb32c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-02-22 13:20:10 -0800

    better explanation

--- 01_26_style_guides.R
+++ 01_26_style_guides.R
@@ -9,13 +9,27 @@
 
 # google's stye guide: https://google.github.io/styleguide/Rguide.xml
 # Hadley Wickham's style guide: http://adv-r.had.co.nz/Style.html
+#   used by RStudio Code > Show Diagnostics
 # Tidyverse style guide: http://style.tidyverse.org/
 # Aroma coding style: http://aroma-project.org/developers/
 # Paul Johnson: https://cran.r-project.org/web/packages/rockchalk/vignettes/Rstyle.pdf
 
+
+# lintr -------------------------------------------------------------------
+
 install.packages("lintr")
+library(lintr)
+# Detailed instructions at https://github.com/jimhester/lintr
+
+lint("../CodeClinicR/2018_1_summarizeTheWeather/CodeClinic_1_weather.R")
+
+
+# formatR -----------------------------------------------------------------
 
 install.packages("formatR")
 library(formatR)
-tidy_source()
 
+newTidyCode <- tidy_source(source = "../CodeClinicR/2018_1_summarizeTheWeather/CodeClinic_1_weather.R")
+newTidyCode$text.tidy
+
+tidy_app()
