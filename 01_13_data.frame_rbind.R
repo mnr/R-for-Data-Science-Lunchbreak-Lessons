b1bd8cb2a950a25ce7939b27bef0db027002ef25
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-03-28 12:18:57 -0700

    added link to the LinkedIn Learning video
    found at https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/data-frames-rbind?trk=insiders_215756_learning

--- 01_13_data.frame_rbind.R
+++ 01_13_data.frame_rbind.R
@@ -1,5 +1,6 @@
 # Copyright Mark Niemann-Ross, 2017
 # Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# Watch the video at https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/data-frames-rbind?trk=insiders_215756_learning
 # LinkedIn: https://www.linkedin.com/in/markniemannross/
 # Github: https://github.com/mnr
 # More Learning: http://niemannross.com/link/mnratlil
