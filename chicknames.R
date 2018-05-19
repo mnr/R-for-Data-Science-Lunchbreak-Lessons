eb31643bb1b06a1b82e1691e6f1f27fa5d395190
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-05-19 07:43:41 -0700

    sync git with LIL

---
+++ chicknames.R
@@ -0,0 +1,12 @@
+# creates a data.frame with ranked names
+# used by data.frame merge example
+chicknames <-
+structure(list(rank = 1:50, name = c("Sophia", "Emma", "Olivia", 
+"Isabella", "Ava", "Mia", "Emily", "Abigail", "Madison", "Elizabeth", 
+"Charlotte", "Avery", "Sofia", "Chloe", "Sammy", "Harper", "Bryce", 
+"Aubrey", "Addison", "Evelyn", "Andrew", "Stephen", "Hannah", 
+"Zoey", "Daniel", "Lillian", "Lily", "Brooklyn", "Samantha", 
+"Todd", "Zoe", "Michael", "Audrey", "Allison", "Anna", "Savannah", 
+"Aaliyah", "Gabriella", "Camila", "Aria", "Kaylee", "Scarlett", 
+"Hailey", "Arianna", "Riley", "Peter", "Nevaeh", "Sarah", "Hulk", 
+"Sadie")), .Names = c("rank", "name"), row.names = c(NA, 50L), class = "data.frame")
