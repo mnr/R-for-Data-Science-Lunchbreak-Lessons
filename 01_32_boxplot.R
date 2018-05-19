ca02b7879ff1ec23be11ad71b111d59cde2e5930
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-05-18 17:52:43 -0700

    initial commits

---
+++ 01_32_boxplot.R
@@ -0,0 +1,15 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# Watch the video at https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/data-frames-rbind?trk=insiders_215756_learning
+# Discuss at https://groups.google.com/d/forum/r-for-data-science-lunchbreak-lessons
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: base graphics: boxplot
+
+boxplot(mtcars$mpg)
+boxplot(mpg ~ cyl, data = mtcars, col = "lightgray", varwidth = TRUE, 
+        main = "mpg vs cylinders",
+        ylab = "mpg",xlab = "cylinders")
+fivenum(mtcars$mpg) # the numbers used to create the boxplot
+
---
+++ 01_37_pieChart.R
@@ -0,0 +1,18 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# Watch the video at https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/data-frames-rbind?trk=insiders_215756_learning
+# Discuss at https://groups.google.com/d/forum/r-for-data-science-lunchbreak-lessons
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: base graphics - pie charts
+
+pie(table(mtcars$cyl))
+
+pie(table(mtcars$cyl),labels = c("Economy","Average","Muscle"))
+
+pie(table(mtcars$cyl),
+    labels = c("Economy","Average","Muscle"), 
+    density = 10*1:3,
+    angle = 100*1:3)
+
