4f93a16668a9e2873dd9ad1829fcdb912d95093b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-07-20 12:42:05 -0700

    ready for recording

--- 01_98_with.R
+++ 01_98_with.R
@@ -8,14 +8,20 @@
 data("ChickWeight")
 
 # original equation
-
-# tableOne <- within(tableOne, quartile <- as.integer(cut(salesPrice, quantile(salesPrice, probs=0:4/4), include.lowest=TRUE)))
-#as.integer(
-cut(ChickWeight$weight, quantile(as.integer(ChickWeight$Diet), probs=0:4/4), include.lowest=TRUE) 
+ChickWeight["quantile"] <- cut(ChickWeight$weight, breaks = quantile(as.integer(ChickWeight$weight)),labels = c("0%", "25%", "50%", "75%"))
 
 # using with()
+ChickWeight["quantile"] <- with(ChickWeight, cut(weight, breaks = quantile(as.integer(weight)), labels = c("0%", "25%", "50%", "75%")))
 
 # using within()
-# tableOne <- within(tableOne, quartile <- as.integer(cut(salesPrice, quantile(salesPrice, probs=0:4/4), include.lowest=TRUE)))
+# The first line doesn't work because within() returns the originating dataframe from the environment
+ChickWeight["quantile"] <-  within(ChickWeight, cut(weight, breaks = quantile(as.integer(weight)), labels = c("0%", "25%", "50%", "75%")))
+# instead, do this...
+ChickWeight <- within(ChickWeight, quantile <- cut(weight, breaks = quantile(as.integer(weight)), labels = c("0%", "25%", "50%", "75%")))
+
 
 # using pipes
+library(magrittr)
+
+ChickWeight["quantile"] <- ChickWeight$weight %>%
+  cut(., breaks = quantile(as.integer(.)), labels = c("0%", "25%", "50%", "75%"))
