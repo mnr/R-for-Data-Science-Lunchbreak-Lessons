0a6be716ace4865727eee9f12e55079b9972daf9
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-07-23 21:54:25 -0700

    initial commit

---
+++ 01_95_dotchart.R
@@ -0,0 +1,14 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# Watch the video at https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/data-frames-rbind?trk=insiders_215756_learning
+# Discuss at https://groups.google.com/d/forum/r-for-data-science-lunchbreak-lessons
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: base::dotchart
+
+data(cars)
+
+dotchart(cars$dist, 
+         groups = factor(cars$speed),
+         main = "Stopping Distance at Different Speeds")
