73ac6396d1e3fbd64a53f9391d70064faf352cdc
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-07-25 09:16:53 -0700

    Initial commit

---
+++ 01_91_ifelse.R
@@ -0,0 +1,32 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: Example file for ifelse
+
+# for example
+if (TRUE) {
+  print("Leghorn")
+} else {
+  print("Orpington")
+}
+
+ifelse(TRUE, "Leghorn", "Orpington")
+# or...
+if (TRUE) "Leghorn" else "Orpington"
+
+# if length(condition) > 1 ...
+MNRChickenRanch <- c(1, 0, 0, 0, 1, 0, 0, 1, NA, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0)
+
+ifelse(MNRChickenRanch, "Leghorn", "Orpington")
+
+# The following fails because length(condition) == 1
+if (MNRChickenRanch) "Leghorn" else "Orpington"
+
+
+# extra credit. I built the random # in MNR Chicken Ranch with this
+ifelse(runif(30) < .5, 0, 1)
+# or...
+sample(c(0,1),size = 30, replace = TRUE)
+# or... look up coin flip...
