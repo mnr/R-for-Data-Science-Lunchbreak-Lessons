8c8fa2827d5aa35b42189da00b130d14126a8dc2
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-07-25 08:46:13 -0700

    initial commit

---
+++ to_be_later/01_92_rattle.R
@@ -0,0 +1,14 @@
+# Copyright Mark Niemann-Ross, 2017
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: Example file for rattle
+
+#  R Analytical Tool To Learn Easily
+# https://rattle.togaware.com/
+# https://cran.r-project.org/web/packages/rattle/index.html
+
+install.packages("rattle")
+library(rattle) # which might ask to install GTK
+rattle()
