54c71282e3094b86afe14487358c10e1308fa78d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-07-26 11:10:43 -0700

    adding comments. specify four gradients instead of three

--- 01_40_pieChart.R
+++ 01_40_pieChart.R
@@ -7,12 +7,15 @@
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: base graphics - pie charts
 
+# simple example of a pie chart
 pie(table(ChickWeight$Diet))
 
+# same pie chart with labeled segments
 pie(table(ChickWeight$Diet),labels = c("horsebean","linseed","soybean", "sunflower"))
 
+# same pie chart, labeled and with gradients instead of colors
 pie(table(ChickWeight$Diet),
     labels = c("horsebean","linseed","soybean", "sunflower"),
-    density = 10*1:3,
-    angle = 100*1:3)
+    density = 10*1:4,
+    angle = 100*1:4)
 
