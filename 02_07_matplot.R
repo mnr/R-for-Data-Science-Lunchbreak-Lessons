34a985e6ea0f2900f29d9cc3d7d5b958f88ce48d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-10-02 18:51:18 -0700

    add devAskNewPage

--- 02_07_matplot.R
+++ 02_07_matplot.R
@@ -9,6 +9,9 @@
 matrix1 <- matrix(c(1:8,1:8), nrow = 8, ncol = 2 )
 matrix2 <- matrix(sample(1:16, 16), nrow = 8, ncol = 2)
 
+devAskNewPage(ask = FALSE) # turns off interactive
+
+# then plot. Plots 1st column of 1st matrix against 2nd column of 2nd matrix
 matplot(matrix1, matrix2)
 matplot(matrix1, matrix2, type = "l")
 matplot(matrix1, matrix2, type = "ls") # other types: p, o, b, h, s
@@ -17,6 +20,6 @@ matplot(matrix1, matrix2, type = "ls",
 
 #adding a second line
 matplot(matrix1, matrix2)
-matlines(matrix(runif(16, min = 1, max = 8), nrow = 8, ncol = 2),
-         matrix(c(1:4,4:1), nrow = 8, ncol = 2))
+matlines(matrix(c(1:16), nrow = 8, ncol = 2),
+         matrix(c(15:0), nrow = 8, ncol = 2))
 
