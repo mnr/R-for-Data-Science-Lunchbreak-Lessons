16ff644dc05742c2208ae43d7a2ea15a42a9dfe5
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-10-03 17:55:31 -0700

    added extra data point to demonstrate three and four point sunflowers

--- 02_12_sunflower.R
+++ 02_12_sunflower.R
@@ -5,13 +5,13 @@
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: sunflower plot
 
-xpos <- c(1,2,3,1,2,3,1,2,3,4,5,6) # 12 xy points
-ypos <- c(1,1,1,1,1,1,1,1,1,1,1,1)
+xpos <- c(1,2,3,3,1,2,3,1,2,3,4,5,6) # 13 xy points
+ypos <- c(1,1,1,1,1,1,1,1,1,1,1,1,1)
 
 # but only 6 points are plotted
 plot(xpos, ypos)
 
-
+# sunflowerplot will show duplicates and the extent they are duplicated
 # use xyTable to calculate overlapping points
 xyTbl <- xyTable(xpos, ypos)
 # then use sunflowerplot to show the overlap at each point
