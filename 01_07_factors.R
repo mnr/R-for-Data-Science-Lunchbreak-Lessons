ecb06121a46fb1b69dc4c5f580abe6bea3f589d0
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-05 14:43:49 -0800

    updated header
    https://linkedin-learning.pxf.io/rweekly_factor

--- 01_07_factors.R
+++ 01_07_factors.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# example files for https://linkedin-learning.pxf.io/rweekly_factor
 # Description: Example file for factors
 
 # Data Structures are * Vectors * Lists * Matrices * Arrays * Data frame * FACTORS
