09679150d1aa614a89c12cf4e86faa3bf13f03a8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-05 15:04:39 -0800

    update header link
    https://linkedin-learning.pxf.io/rweekly_matrix

--- 01_08_matrix.R
+++ 01_08_matrix.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# example file for https://linkedin-learning.pxf.io/rweekly_matrix
 # Description: Example file for Matrix: five minutes of R
 
 # Data Structures are * Vectors * Lists * MATRICE * Arrays * Data Frame * factors
