0822f396b383262f7c1d9bfaae45e83ce21e519c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-09 17:52:54 -0800

    simplify header
    https://linkedin-learning.pxf.io/rweekly_equalIn

--- 01_45_sets_equal_in.R
+++ 01_45_sets_equal_in.R
@@ -1,9 +1,4 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
-# Description: Example file for sets
+# example for LinkedIn Learning Video: https://linkedin-learning.pxf.io/rweekly_equalIn
 
 # create two vectors to demonstrate
 vectA <- c(1:10)
