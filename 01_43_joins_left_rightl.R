f390e16e941084299ca114ea87bd167ed9747b1c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-15 15:09:30 -0800

    cleaned up header and link
    https://linkedin-learning.pxf.io/rwkly_leftrightjoin

--- 01_43_joins_left_rightl.R
+++ 01_43_joins_left_rightl.R
@@ -1,8 +1,5 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# example file for LinkedIn Learning video https://linkedin-learning.pxf.io/rwkly_leftrightjoin
+
 # Description: Example file for joins
 
 # Need some data to play with
