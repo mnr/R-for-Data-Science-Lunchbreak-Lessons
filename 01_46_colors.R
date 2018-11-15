c1a11b928b912cca719389cb596e1d28021cc820
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-14 22:31:10 -0800

    added link to video course
    Colors! For colors in R you'll need colorRampPalette. Here's how.
    https://linkedin-learning.pxf.io/rwkly_colors

--- 01_46_colors.R
+++ 01_46_colors.R
@@ -1,8 +1,5 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+
+# Example code for LinkedIn Learning course at https://linkedin-learning.pxf.io/rwkly_colors
 # Description: Example file for colors
 
 # main idea - I need some colors that won't hurt my eyes
