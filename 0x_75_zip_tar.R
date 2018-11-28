a37d231f97911738d6a2e2da0621593d0c581580
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-28 15:33:22 -0800

    Initial commit

---
+++ 0x_75_zip_tar.R
@@ -0,0 +1,19 @@
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: zip, unzip, tar, untar
+
+
+# may not work on windows if no zip program available
+# this should return a path to a zip command
+# if it doesn't, this command won't work.
+# You'll need to install a zip program
+Sys.getenv("R_ZIPCMD") 
+
+# make sure current directory is exercise files
+zip("aZipFile.zip", "01_01_builtin_datasets.R")
+
+unzip("aZipFile.zip")
+unzip("aZipFile.zip", list = TRUE) # lists contents of zip file
