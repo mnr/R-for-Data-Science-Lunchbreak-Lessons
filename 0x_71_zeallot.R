79d8dfaec400366f5a497cf0b372e6eb437be0c1
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-27 16:55:57 -0800

    Initial commit

---
+++ 0x_71_zeallot.R
@@ -0,0 +1,23 @@
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: %<-% tuples via zeallot
+
+install.packages("zeallot")
+library(zeallot)
+
+# similar to python's ability to : x,y = 1,2. x = 1. y = 2
+
+c(firstVar, secondVar) %<-% c(1,2)
+
+c(firstVar, secondVar) %<-% c("a string",2) # both values are assigned as strings
+
+# break data frames into vectors
+c(NewWeight, newTime, newChick, newDiet) %<-% ChickWeight
+
+# return multiple values from a function
+myFunction <- function() { return(c(2,3))}
+c(firstVar, secondVar) %<-% myFunction()
+
