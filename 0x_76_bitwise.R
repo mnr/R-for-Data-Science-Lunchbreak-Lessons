762ff3135834f5ea31d99b5b5a6bf33a8fb48cd4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-11-29 10:36:56 -0800

    added comments about bytes, nibbles, western arabic

--- 0x_76_bitwise.R
+++ 0x_76_bitwise.R
@@ -5,10 +5,19 @@
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: bitwise operators
 
+# bitwise operators are included in R, but mostly for compatibility with other languages.
+# microbenchmark indicates this is significantly slower than the normal operator
 # what are bitwise operators?
 
 intToBits(8) # = 00 00 00 01
 intToBits(1) # = 01 00 00 00
+
+# normal binary notation for eight would be 00 00 10 00. 
+# least significant bit on right (same as western arabic numbering)
+# R reverses the nibbles of the bytes and the most/least significant bit order
+# byte = 8 bits
+# nibble = 1/2 byte
+
 bitwAnd(8,1) # = 00 00 00 00 = 0
 bitwOr(8,1)  # = 01 00 00 01 = 9
 bitwXor(8,1) # = 01 00 00 01 = 9
@@ -22,3 +31,14 @@ bitwShiftL(8, 2) # = 32 = x*4
 bitwShiftR(8, 1) # = 4  = x/2
 
 
+# are bitwise operators faster? -------------------------------------------
+
+library(microbenchmark)
+
+microbenchmark(
+  bitwShiftL(2000000,6), # median = 1539 nanoseconds
+  2000000 * 64,          # median = 1 nanosecond
+  times = 1000
+)
+
+# answer...probably not
