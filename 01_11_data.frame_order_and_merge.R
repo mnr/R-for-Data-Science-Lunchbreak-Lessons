915363765976fe2070e78edbc70db9243373a060
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-12-05 12:34:01 -0800

    add link to linkedin learning course
    https://linkedin-learning.pxf.io/rweekly_sortmerge

--- 01_11_data.frame_order_and_merge.R
+++ 01_11_data.frame_order_and_merge.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# exercise files for LinkedIn Learning: https://linkedin-learning.pxf.io/rweekly_sortmerge
 # Description: Example file for Dataframes: order and merge
 
 # main idea: sorting and merging data.frames
