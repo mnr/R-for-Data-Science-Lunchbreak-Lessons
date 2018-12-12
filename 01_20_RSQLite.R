57a4a260e478670108ac9ac1c40e3d08bb89aced
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-12-12 10:10:02 -0800

    add link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_rsqlite

--- 01_20_RSQLite.R
+++ 01_20_RSQLite.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# Example files for LinkedIn Learning: https://linkedin-learning.pxf.io/rweekly_rsqlite
 # Description: Example file for RSQLite
 
 # main idea: Use SQLite to store/buffer data. it's persistent and uses SQL
