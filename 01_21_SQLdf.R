15a1f3544d88be5e769825ade8d6880d2b2972e8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-12-20 09:36:14 -0800

    add link to LinkedIn Video Lesson
    https://linkedin-learning.pxf.io/rweekly_sqldf

--- 01_21_SQLdf.R
+++ 01_21_SQLdf.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2017
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# link to LinkedIn Lesson: https://linkedin-learning.pxf.io/rweekly_sqldf
 # Description: Example file for SQLdf
 
 # main idea: crossover between R data.frames and SQL
