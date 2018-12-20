01fd9be38ef1efc8728174fd4218a5ddc913c482
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-12-20 09:32:56 -0800

    Add link to video
    https://linkedin-learning.pxf.io/rweekly_mirror

--- 01_51_defaultMirror.R
+++ 01_51_defaultMirror.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# Link to LinkedIn Learning Video: https://linkedin-learning.pxf.io/rweekly_mirror
 # Description: setting the default mirror
 
 # installing packages at the command line may generate an error.
