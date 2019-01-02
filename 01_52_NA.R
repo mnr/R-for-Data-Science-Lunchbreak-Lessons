8369ef19ab63fb20ccfc0d98f5f903b5a17206ee
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-01-02 11:30:05 -0800

    link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_na

--- 01_52_NA.R
+++ 01_52_NA.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# link to LinkedIn Learning Course: https://linkedin-learning.pxf.io/rweekly_na
 # Description: dealing with NA
 
 ?NA
