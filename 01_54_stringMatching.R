3284fabb510f88a13d09285faaab78d83f5f0739
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-01-23 11:06:18 -0800

    add link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_simplestringmatch

--- 01_54_stringMatching.R
+++ 01_54_stringMatching.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# Example files for LinkedIn Learning https://linkedin-learning.pxf.io/rweekly_simplestringmatch
 # Description: string matching
 
 haystack <- c("red", "blue", "green", "blue", "green forest")
