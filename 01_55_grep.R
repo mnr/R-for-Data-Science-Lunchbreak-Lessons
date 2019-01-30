531c508637a6aba50b0229b0fbbb028cb7e415a8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-01-30 12:29:28 -0800

    add link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_grep

--- 01_55_grep.R
+++ 01_55_grep.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# Exercise file for LinkedIn Learning: https://linkedin-learning.pxf.io/rweekly_grep
 # Description: grep - global regular expression print
 
 haystack <- c("red", "blue", "green", "blue", "green forest")
