dc05e3a7c80c273e8c8ceb0ca6fca7fef6767d90
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-02-06 11:21:47 -0800

    link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_dotchart

--- 01_56_dotchart.R
+++ 01_56_dotchart.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# Example file for LinkedIn Learning video at https://linkedin-learning.pxf.io/rweekly_dotchart
 # Description: dotchart
 
 # dotchart works with matrix or vector (not dataframe)
