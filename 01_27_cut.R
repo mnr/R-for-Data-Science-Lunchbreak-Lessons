b85d4ab4286abb387e59bc1c8d50557ca1f668cf
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-02-06 12:08:00 -0800

    link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_cut

--- 01_27_cut.R
+++ 01_27_cut.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# Example file for https://linkedin-learning.pxf.io/rweekly_cut
 # Description: cut to set intervals
 
 numericVector <- runif(100, min = 1, max = 256 )
