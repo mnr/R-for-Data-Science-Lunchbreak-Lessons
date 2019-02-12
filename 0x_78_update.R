923986c4d4e56b7de718142687485f193fe156c2
Mark Niemann-Ross <mark@niemannross.com>
2019-02-12 11:17:54 -0800

    Initial commit

---
+++ 0x_78_update.R
@@ -0,0 +1,19 @@
+# Copyright Mark Niemann-Ross, 2019
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: update your copy of R
+
+# best to do this from R - NOT RStudio
+
+install.packages("installr") 
+library(installr)
+updateR()
+
+# What happens when packages are copied from old directory to new?
+# - copied to new directory
+# - option to delete old packages
+# - option to update new packages
+# Do you wish to keep these packages? 
+# answer: depends on your work environment. Possibly some packages won't work with new R
