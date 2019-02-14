f03bd74fc9a997139a6b01c5ad4cfc13a50a020c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-02-13 21:30:51 -0800

    link to linkedin learning
    https://linkedin-learning.pxf.io/rweekly_fourfoldplot

--- 01_57_fourfoldplot.R
+++ 01_57_fourfoldplot.R
@@ -1,8 +1,4 @@
-# Copyright Mark Niemann-Ross, 2018
-# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
-# LinkedIn: https://www.linkedin.com/in/markniemannross/
-# Github: https://github.com/mnr
-# More Learning: http://niemannross.com/link/mnratlil
+# example file for LinkedIn Learning course at https://linkedin-learning.pxf.io/rweekly_fourfoldplot
 # Description: fourfoldplot
 
 # Basic Idea ----
