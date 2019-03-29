30c8deba3ec58eb8fb201d91c1e7d03f93b9fee3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-03-28 22:47:59 -0700

    initial commit

---
+++ 0x_80_protectYourPasswords.R
@@ -0,0 +1,23 @@
+# Copyright Mark Niemann-Ross, 2019
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: don't include your password in R
+
+# Store unencrypted password
+file.edit(file.path("~", ".Renviron"))
+
+# in .Renviron, put key="value"...
+# R_myPassword="something Secret"
+
+Sys.getenv("R_myPassword") # retrieve it
+
+# or...Store your passwords on an encrypted disk or removable drive
+# create a file on external drive
+# or on cloud storage such as onedrive, google drive or dropbox
+# the file contains password definitions. 
+# such as ... mySecretPassword <- "Waldorf salad"
+# Warning: this will show up in a list of environmental variables
+source("mysecrets.r")
+
