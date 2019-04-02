e359b57ba5abb3ea1a83bb8b883ca5a69d61e92b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-04-02 12:28:40 -0700

    Initial commit

---
+++ 0x_83_stopifnot.R
@@ -0,0 +1,28 @@
+# Copyright Mark Niemann-Ross, 2019
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: stopifnot
+
+# stopifnot is good for testing multiple expressions for true or false
+
+trueExpression <- TRUE
+falseExpression <- FALSE
+enoughBaskets <- function(fruits, baskets) {
+  return(fruits <= baskets)
+}
+
+# stopifnot signals which was false
+stopifnot(trueExpression, enoughBaskets(3,5), enoughBaskets(5,3))
+
+# use to check validity of parameters in an expression
+# throws an error condition. 
+enoughBaskets <- function(fruits, baskets) {
+  stopifnot(fruits <= baskets)
+  # we can proceed with confidence that fruit will fit in baskets
+}
+enoughBaskets(3,5)
+enoughBaskets(5,3)
+
+# hint: try "options(error = recover)" to go to debugger
---
+++ 0x_84_weightedmean.R
@@ -0,0 +1,26 @@
+# Copyright Mark Niemann-Ross, 2019
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: weighted.mean
+
+# which fruit basket is better?
+# what I like
+apples <- 1
+peaches <- 2
+brusselsprouts <- -1
+fruitWeights <- c(apples, peaches, brusselsprouts)
+
+# contents of basket
+BobsBasket <- c(apples=10, peaches=5, brusselsprouts = 5)
+JanesBasket <- c(apples=5, peaches=10, brusselsprouts = 0)
+
+# just a mean
+mean(BobsBasket)
+mean(JanesBasket)
+# bob vs jane, 6.6 vs 5. Bob's basket is better!
+# wait a minute!
+weighted.mean(BobsBasket, fruitWeights)
+weighted.mean(JanesBasket, fruitWeights)
+# bob vs jane, 7.5 vs 12.5. Jane's basket is better!
