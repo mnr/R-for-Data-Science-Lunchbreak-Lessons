90180be5773ec714063054feaa01d1cfcb14ef2c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-05-15 15:44:26 -0700

    add macintosh updateR

--- 0x_78_update.R
+++ 0x_78_update.R
@@ -7,9 +7,18 @@
 
 # best to do this from R - NOT RStudio
 
+# for windows
 install.packages("installr") 
 library(installr)
 updateR()
+
+# for macintosh
+install.packages('devtools') #assuming it is not already installed
+library(devtools)
+install_github('andreacirilloac/updateR')
+library(updateR)
+updateR(admin_password = 'Admin user password')
+
 
 # What happens when packages are copied from old directory to new?
 # - copied to new directory
