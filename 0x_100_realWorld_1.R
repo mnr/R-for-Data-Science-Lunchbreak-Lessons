9c2a28a3e8ccc9251558c14b2ea6c2a71dbd6f28
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-06-25 15:55:50 -0700

    add links to r weekly on linkedin learning

--- 0x_100_realWorld_1.R
+++ 0x_100_realWorld_1.R
@@ -1,12 +1,14 @@
 # Using R in the real world
 # Challenge: https://www.linkedin.com/learning/statistics-foundations-1/understanding-statistics-with-the-use-of-charts
 
+# see https://www.linkedin.com/learning/r-programming-in-data-science-high-variety-data/read-individual-cells-from-excel
 install.packages("readxl")
 library(readxl)
 
 
 # download and unpack the zip file for course
 # then run this to find "Chapter 1"
+# file.choose r weekly not listed yet
 statExcelFile <- file.choose()
 
 # read the range from the excel file 
@@ -14,14 +16,15 @@ statData <- read_xlsx(statExcelFile, range = "WEIGHTS!b2:B52")
 
 
 # create a dotplot of weight ~ frequency
+# https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/stripchart
 stripchart(statData$Weights, method = "stack", offset = 0.5, at = 0.2, 
            pch = 16, cex = 4)
 
 # Create a histogram of 10 pound intervals
+# https://www.linkedin.com/learning/r-for-data-science-lunchbreak-lessons/histogram
 lowest_ten <- as.integer(min(statData$Weights)/10) * 10
 weightbyten <- seq(from = lowest_ten-20, to = max(statData$Weights)+20, by = 10)
-brokenWeights <- cut(statData$Weights, breaks = weightbyten)
-hist(table(brokenWeights), xlab = levels(brokenWeights))
+#brokenWeights <- cut(statData$Weights, breaks = weightbyten)
 
 hist(statData$Weights, breaks = weightbyten, right = FALSE)
 
