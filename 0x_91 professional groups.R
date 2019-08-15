07c835a62f16af50930b7c24d082effd6b6c24b9
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-08-15 13:25:10 -0700

    some sites have gone down

--- 0x_91 professional groups.R
+++ 0x_91 professional groups.R
@@ -1,18 +1,20 @@
 # R ladies and other networking groups
 
 # R Ladies Website
-# https://rladies.org
-
-# Interactive shiny presentation
-# https://gqueiroz.shinyapps.io/rshinylady/
+https://rladies.org
 
 # a list of R conferences
-# official list - https://www.r-project.org/conferences/
-# complete list - https://jumpingrivers.github.io/meetingsR/index.html
-# user groups - https://jumpingrivers.github.io/meetingsR/r-user-groups.html
+# official list - 
+https://www.r-project.org/conferences/
+# complete list - 
+https://jumpingrivers.github.io/meetingsR/index.html
+# user groups - 
+https://jumpingrivers.github.io/meetingsR/r-user-groups.html
   
 # LinkedIn Group
-# https://www.linkedin.com/groups/77616/
+# 
+https://www.linkedin.com/groups/77616/
 
 # A great resource for information
-# https://www.r-bloggers.com
+# 
+https://www.r-bloggers.com
