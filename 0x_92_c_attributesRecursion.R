9da7aed84acfed9e08cced5c13cbd452ad16eea6
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-08-15 13:25:33 -0700

    added definitions for arrayThing and dfThing

--- 0x_92_c_attributesRecursion.R
+++ 0x_92_c_attributesRecursion.R
@@ -1,9 +1,15 @@
+arrayThing <- array(c(1:300), dim = c(10,10,3))
+dfThing <- data.frame(letters = letters[1:10], numbers = 1:10, stringsAsFactors = FALSE )
+
 # using c( ) to remove attributes
+str(arrayThing)
 vectorThing <- c(arrayThing) # removes attributes
 str(vectorThing)
+
+str(dfThing)
 vectorThing <- c(dfThing) # simplifies to list
 
 # recursive simplifies to vector, retaining names
 vectorThing <- c(dfThing, recursive = TRUE) 
 vectorThing
-vectorThing["alpha13"]
+vectorThing["letters9"]
