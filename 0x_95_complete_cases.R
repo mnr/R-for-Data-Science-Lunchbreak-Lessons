09ddcff0589df972eaae0322bbc309d193730305
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-08-16 08:20:27 -0700

    use nasa abbreviations instead of fda abbreviation

--- 0x_95_complete_cases.R
+++ 0x_95_complete_cases.R
@@ -6,25 +6,20 @@
 # Description: 
 
 # complete.cases finds lines with missing data
-
-# grab a copy of the FDA.gov Acronyms & Abbreviations
-download.file(url = "https://www.fda.gov/media/71010/download", mode = "wb" , destfile = "Acronym.zip")
-unzip("Acronym.zip")
-Acronym_export <- read.delim("Acronym_export.txt", stringsAsFactors=FALSE)
-Acronym_export$Definition <-  trimws(Acronym_export$Definition, which = "both")
-Acronym_export$Acronym <-  trimws(Acronym_export$Acronym, which = "both")
+load("nasa.rds")
 
 # example of a complete case
-Acronym_export[30,]
+nasa_abbreviations[65,]
 
 # example of incomplete case
-Acronym_export[3612,] # missing all three variables
-Acronym_export[3758,] # Has a definition, but it's a number?
+nasa_abbreviations[66,] # missing definition
+nasa_abbreviations[69,] # missing abbreviation
 
 # so - how to get rid of incomplete cases?
 # use complete.cases
-complete.cases(Acronym_export) # returns TRUE or FALSE
-sum(complete.cases(Acronym_export)) #3809 complete cases
-Acronym_export[complete.cases(Acronym_export), ] # returns only complete cases
-Acronym_export[!complete.cases(Acronym_export), ] # returns only incomplete cases
+complete.cases(nasa_abbreviations) # returns TRUE or FALSE
+nrow(nasa_abbreviations) # 82 cases
+sum(complete.cases(nasa_abbreviations)) #80 complete cases
+nasa_abbreviations[complete.cases(nasa_abbreviations), ] # returns only complete cases
+nasa_abbreviations[!complete.cases(nasa_abbreviations), ] # returns only incomplete cases
 
