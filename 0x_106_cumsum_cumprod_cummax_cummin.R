f33d7346919fc0eea533fa36e61f6ccd071b2c28
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-08-22 13:09:17 -0700

    Initial commit of cumsum cumprod cummax cummin

---
+++ 0x_106_cumsum_cumprod_cummax_cummin.R
@@ -0,0 +1,38 @@
+# Copyright Mark Niemann-Ross, 2019
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: cumulative operators
+
+# cumulative summation
+cumsum(1:3) # 1 + 2 + 3 = 6
+
+cumsum(1,2,3) # nope. has to be one arguement
+
+cumsum(c(1:3)) # return value matches length of input value
+
+cumsum(c(1,2,3,NA,4)) # NA interrupts the sum
+
+
+# cumprod just like cumsum
+cumprod(1:3)
+
+# cummin and cummax behave as expected
+cummin(1:3)
+cummax(1:3)
+
+cummax(c(1:3,2))
+cummax(c(1:3,NA)) # NA is a killer
+
+# A plot to see the relationship
+myVector <- 1:20
+myVectorcumSum <- cumsum(myVector)
+randomMyVector <- sample(myVector,length(myVector) )
+
+plot(myVectorcumSum, type = "l")
+lines(cummax(randomMyVector))
+points(myVector)
+text(x = length(myVector), y = c(max(myVector),max(myVectorcumSum)), 
+     labels = c("cummax", "cumsum"), 
+     adj = c(1,0))
