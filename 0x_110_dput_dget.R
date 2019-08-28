3b91d27be45961040f5e22082d645f82070c6dd3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-08-28 15:48:05 -0700

    Rename to 110 and initial commit

---
+++ 0x_110_dput_dget.R
@@ -0,0 +1,26 @@
+# Copyright Mark Niemann-Ross, 2019
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: 
+
+# dput will write an R language version of an R object to disk
+dput(ChickWeight, file = "chickweight_dput.R")
+# includes meta data
+
+# compare to save
+save(ChickWeight, file = "chickweight_save.rds")
+# save creates a binary version of the file
+
+# file.write would create a csv
+write.csv(ChickWeight, file = "chickweight_writecsv.csv")
+# does not include meta data or ability to buid the file programmatically 
+
+# retrieve a dput file.
+# either run the dput code
+# ... or ...
+newChickWeight <- dget(file = "chickweight_dput.R")
+
+
+# dput & dget might not work. Use save and load to transfer R objects
