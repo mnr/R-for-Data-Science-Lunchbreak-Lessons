9b604515ebeca760b09fca444ab8cac52152ec67
Mark Niemann-Ross <mark.niemannross@gmail.com>
2019-10-21 11:15:21 -0700

    adding some presentation comments

--- 0x_102_repeatloop.R
+++ 0x_102_repeatloop.R
@@ -5,24 +5,26 @@
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: repeat loop 
 
-# repeat just loops forever. You'll need break
+# repeat is one of many flow controls. (i.e. While, for.., if...then)
 
-bobsYourUncle <- 1
+# unlike other flow controls, repeat DEFAULTS to an endless loop
+bobsYourUncle <- 1 # so create a counter
 
 repeat {
   print(bobsYourUncle)
   bobsYourUncle <- bobsYourUncle + 1
-  if(bobsYourUncle > 10) {break}
+  if(bobsYourUncle > 10) {break} # do this, or be here forever
 }
 
-# This is considered better programming
+# This is considered better programming since intent is obvious
 bobsYourUncle <- 1
 while (bobsYourUncle < 11) {
   print(bobsYourUncle)
   bobsYourUncle <- bobsYourUncle + 1
 }
 
-# but perhaps you're testing multiple vectors...
+# so...when to use repeat
+# perhaps you're testing multiple vectors where and or or will be confusing
 
 repeat {
   if (rnorm(1) > 3.8) break # ... if random number greater than 3.8
