7940948eaea11d64e0e440d5898506b9ebe684b2
Mark Niemann-Ross <mark.niemannross@gmail.com>
2020-01-09 18:23:47 -0800

    in process

--- 0x_128_regmatches.R
+++ 0x_128_regmatches.R
@@ -1,3 +1,7 @@
 # Find and replace substrings with regmatches()	regmatches uses regular
 # expressions to find and replace substrings of other strings. Text manipulation
 # is a strong feature of the R language.
+
+someText <- "Twas brillig and the slithey toves bought 15 apples to the market."
+
+regmatches(someText, gregexpr("b\\w+", someText))
