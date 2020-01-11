e8417db352315bed2c29abca5024b18437673c0e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2020-01-10 17:04:02 -0800

    Changing 119 from rcmd to rep

--- 0x_119_rmd.R
+++
@@ -1,3 +0,0 @@
-# Use rmd for documentation	Rmd is an R version of markdown, a common format for
-# writing documentation. Learning to create reproducible and documented course
-# is essential for data scientists.
