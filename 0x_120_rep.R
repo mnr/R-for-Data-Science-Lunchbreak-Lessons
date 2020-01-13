45d68979c146f142a03ef99f599e42c03e034787
Mark Niemann-Ross <mark.niemannross@gmail.com>
2020-01-12 21:49:17 -0800

    initial commit

--- 0x_120_rep.R
+++ 0x_120_rep.R
@@ -1,3 +1,28 @@
 # Use rep() to create long repetitive vectors	rep is easy, but oh-so-useful for
 # repetitive strings. Using rep to create long repetitive strings is an
 # essential for big data.
+
+# simply...
+rep("Hello", 5)
+
+# ... or...
+rep("hello", length.out = 10)
+
+# repeats vector elements
+rep(c("hello","bob"), 3)
+
+# repeats each element in the vector
+rep(c("hello","bob"), each = 3)
+
+# what happens if you repeat a dataframe
+thisdf <- data.frame(Bob = c(4,5,6),
+                     Shirley = c(7,8,9))
+
+anotherdf <- rep(thisdf,3) # you get a list
+
+# here's a complex example
+# I need Quarter identifiers for an unpredictable number of years
+paste0(2003:2021,"-") # range of years
+rep(paste0(2003:2021,"-"), 4) # 4x years - one for each quarter
+sort(rep(paste0(2003:2021,"-"),4)) # sort by year
+paste0(sort(rep(paste0(2003:2021,"-"),4)),c("Q1","Q2","Q3","Q4"))
