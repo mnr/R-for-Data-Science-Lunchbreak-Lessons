bf5962a8a449654e0e8db17f24262e2e07ebb1a5
Mark Niemann-Ross <mark@niemannross.com>
2020-01-14 23:41:54 -0800

    initial commits

--- 0x_130_par.R
+++ 0x_130_par.R
@@ -1,1 +1,9 @@
-# par
+# par for graph margins
+
+par(mar = c(5.1, 4.1, 4.1, 2.1)) # default values
+# bottom, left, top, right margins
+
+par(mai = c(1.02, 0.82, 0.82, 0.42))
+
+# default graph size
+par("fin")
