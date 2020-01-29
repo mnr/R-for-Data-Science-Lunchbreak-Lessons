543d9bed218da60b9ffcfb399ed1f6c2b2ecb75a
Mark Niemann-Ross <mark.niemannross@gmail.com>
2020-01-29 13:34:59 -0800

    add affiliate links

--- 01_64_switch.R
+++ 01_64_switch.R
@@ -4,6 +4,7 @@
 # Github: https://github.com/mnr
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: switch
+# affiliate: https://linkedin-learning.pxf.io/rweekly_switch
 
 # Switch is a way to create multiple actions to decisions
 
