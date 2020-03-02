73560317c732d800f64db3917dfabe1eea2a60e6
Mark Niemann-Ross <mark@niemannross.com>
2020-03-02 15:06:07 -0800

    add affiliate links

--- 01_90_expand_grid.R
+++ 01_90_expand_grid.R
@@ -4,6 +4,7 @@
 # Github: https://github.com/mnr
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: 
+# affiliate: https://linkedin-learning.pxf.io/rweekly_expandGrid
 
 #compare expand.grid vs combn
 
--- 01_93_logical_operators.R
+++ 01_93_logical_operators.R
@@ -4,6 +4,7 @@
 # Github: https://github.com/mnr
 # More Learning: http://niemannross.com/link/mnratlil
 # Description: logical operators
+# affiliate: https://linkedin-learning.pxf.io/rweekly_logOps
 
 # common to a lot of languages, but worth reviewing
 
