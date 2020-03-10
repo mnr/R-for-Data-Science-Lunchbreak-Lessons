bb58e3f8c135d41f27f508985d39495831b71c23
Mark Niemann-Ross <mark.niemannross@gmail.com>
2020-03-10 13:17:22 -0700

    initial commit

--- 0x_131_exists.R
+++ 0x_131_exists.R
@@ -1,1 +1,13 @@
 # exists - look for an r object
+
+# test if an object exists in the current environment
+
+if(exists("DefinedVector")) {
+  # avoid repeating long and expensive routine
+  print("It's already defined!")
+} else {
+  print("It's NOT defined")
+  # go ahead and do the expensive routine
+}
+
+DefinedVector <- "Here I am!"
