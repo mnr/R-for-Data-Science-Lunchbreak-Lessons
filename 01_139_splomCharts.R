cb31b81915a04404e72d7999e35f2abc4473a68a
Mark Niemann-Ross <mark@niemannross.com>
2020-04-28 18:04:38 -0700

    renamed and recorded

--- 01_139_splomCharts.R
+++ 01_139_splomCharts.R
@@ -5,11 +5,9 @@
 
 library(lattice)
 
-str(ChickWeight) # so we can see what the formula is doing
+splom(ChickWeight)
 
-splom(~ChickWeight)
-
-splom(~ChickWeight[1:2]) # plots weight & time
+splom(ChickWeight[1:2]) # plots weight & time
 
 splom(~ChickWeight[1:2], 
       groups = Diet, 
@@ -20,6 +18,7 @@ splom(~ChickWeight[1:2],
       groups = Diet, 
       data = ChickWeight,
       panel = panel.smoothScatter )
+
 
 # colorful - but is it useful?
 parallelplot(~ChickWeight[1:2], 
