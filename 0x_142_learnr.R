e74b9c4a3efee0f63b73be75215ac4731971d7a2
Mark Niemann-Ross <mark@niemannross.com>
2020-05-20 17:28:29 -0700

    1st notes

--- 0x_142_learnr.R
+++ 0x_142_learnr.R
@@ -1,1 +1,18 @@
-# learnr
+# learnr - https://rstudio.github.io/learnr/
+
+# learnr is a tool for creating R lessons
+# similar to swirl - which we discussed earlier
+# but easier - based on Rmd (r markdown)
+
+install.packages("learnr")
+
+# create a new document
+# file::new file::R markdown::from template::interactive tutorial
+
+# lines 3 & 4 of the new file indicate this is a learnr document
+output: learnr::tutorial
+runtime: shiny_prerendered
+
+# look at top of code window - run document...
+
+# for new exercise, {r, exercise=TRUE}
