8ee03ac34c3f483fb6d13fda82d0b54caa97c4fe
Mark Niemann-Ross <mark@niemannross.com>
2020-05-28 17:07:45 -0700

    adding identifier plot points

--- 0x_144_par_margins.R
+++ 0x_144_par_margins.R
@@ -1,24 +1,48 @@
 # using par to set margins and layout
 
 # need to save current status of par
+par(omi = c(.5, .5, .5, .5))
+
 oldParValues <- par(no.readonly = TRUE)
 
 # here's a diagram of plot regions
-par(omi = c(.8, .8, .8, .8))
-makeAPlot <- function() {
-  plot(1:10, main = "main title", xlab = "This is the x label", ylab = "This is the y label", sub = "Subtitle")+
+
+makeAPlot <- function(showThis = "labels") {
+  plot(1:10, xlab = "", ylab = "", sub = "", axes = FALSE)+
     box(which = "outer",  col = "red")+
-    mtext("The red line is the edge of the device region", side = 3, line = 3, outer = TRUE)+
-    mtext("This area is the outer margin", side = 3, line = 1, outer = TRUE)+
     box(which = "inner",  col = "green")+
-    mtext("The green line is the figure region", side = 3, line = 3)+
-    mtext("This area is the figure/inner region", side = 3, line = 1)+
     #box(which = "figure", col = "blue")+
-    mtext("The yellow box is the edge of the plot")+
     box(which = "plot", col = "yellow")
+  if (showThis == "labels") {
+    mainText = "Show Labels"
+    mtext("The red line is the edge of the device region", side = 3, line = 3, outer = TRUE)
+    mtext("This area is the outer margin", side = 3, line = 1, outer = TRUE)
+    mtext("The green line is the figure region", side = 3, line = 3)
+    mtext("This area is the figure/inner region", side = 3, line = 1)
+    mtext("The yellow box is the edge of the plot")
+  } else if (showThis == "functions") {
+    mainText = ""
+    mtext(paste('par("din")[1] =', prettyNum(par("din")[1], digits = 3)), 
+          line = 1, outer = TRUE, adj = 1 )
+    mtext(paste('par("din")[2] =', prettyNum(par("din")[2], digits = 3)), 
+          line = 1, outer = TRUE, adj = 1, side = 1 )
+    for (theIndex in 1:4) {
+      mtext(paste0('par("omi")[',theIndex,'] =', prettyNum(par("omi")[theIndex], digits = 3) ),
+            line = 3, adj = 0, side = theIndex)
+      mtext(paste0('par("mai")[',theIndex,'] =', prettyNum(par("mai")[theIndex], digits = 3) ),
+            line = 0, adj = 0, side = theIndex)
+
+    }
+    
+  } else if (showThis == "values") {
+    
+  }
+  title(main = mainText)
+
 }
 
-makeAPlot()
+makeAPlot(showThis = "functions")
+makeAPlot(showThis = "labels")
 
 # margins are specified in lines of text or inches.
 par("oma") # lines of text: bottom, left, top, right
@@ -55,6 +79,13 @@ par("mar") # figure margin / lines (bottom, left, top, right)
 par("mai") # figure margin / inches (bottom, left, top, right)
 
 par(mai = c(.2,.2,.2,.2))
+makeAPlot()
+par(oldParValues) # reset par
+
+# par("fig") # coordinates of figure region compared to par("din")
+par("fin") # size of figure region in inches. dynamic
+
+par(fin = c(4,3))
 makeAPlot()
 par(oldParValues) # reset par
 
