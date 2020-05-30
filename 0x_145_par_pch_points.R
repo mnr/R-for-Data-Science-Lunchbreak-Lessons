940bfa2983c457c8372d7259fe07d155a5d24c57
Mark Niemann-Ross <mark@niemannross.com>
2020-05-29 17:57:36 -0700

    initial commit

---
+++ 0x_145_par_pch_points.R
@@ -0,0 +1,17 @@
+# pch - lots of points
+
+plot(1:10)
+par("pch")
+par(pch = 10)
+plot(1:10)
+
+# or...
+plot(1:10, pch = 12)
+plot(1:10, pch = 12, cex = 3)
+
+# what points are available?
+# specified in points
+plot(1:10) + points(x = 10, y = 1, pch = 15)
+
+# run the code from points to see a graphic
+example("points")
