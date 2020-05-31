8485fac9fee53ed927981435f3f757c29ffb3d0e
Mark Niemann-Ross <mark@niemannross.com>
2020-05-31 12:38:30 -0700

    initial script

--- 0x_153_locator.R
+++ 0x_153_locator.R
@@ -1,1 +1,22 @@
-locator(20)
+# interactive plotting
+
+# install.packages("jpeg")
+library(jpeg)
+myImage <- as.raster(readJPEG("0301771_small.jpg"))
+plot(1:10)
+rasterImage(myImage, 
+            xleft = 0,
+            ybottom = 0,
+            xright = 100,
+            ytop = 100)
+# now that I've drawn the raster image, I can count stars
+locator(10)
+
+# does this work on your system?
+dev.capabilities("locator")
+
+# connector type. default = n (none). p= points, l=lines, o=both, 
+locator(10, type = "o")
+# or add other par() values
+locator(10, type = "o", col = "yellow", lwd = 3)
+
