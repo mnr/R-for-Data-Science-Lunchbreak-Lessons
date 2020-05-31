a24f18152e9088a933427f7318394d6d74ca0dd8
Mark Niemann-Ross <mark@niemannross.com>
2020-05-31 11:27:33 -0700

    rename & renumber

--- to_be_later/0x_153_factorial.R
+++ to_be_later/0x_153_factorial.R
@@ -1,7 +1,6 @@
 # factorial
 
-# this is mostly a placeholder
-
-
 n <- 3 # for an n x n matrix
 factorial(n) # provides the number of permutations
+plot(factorial(1:5), type = "l")
+plot(gamma(1:5), type = "l")
