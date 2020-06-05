f0d63db15c331320b4645062bb32e598b0956ace
Mark Niemann-Ross <mark@niemannross.com>
2020-06-05 12:30:57 -0700

    Clean up the discussion

--- 0x_155_crossproduct.R
+++ 0x_155_crossproduct.R
@@ -1,24 +1,26 @@
 # crossproduct & tcrossproduct
 
-# placeholder. Needs elaboration
+A <- matrix( c(1:9), nrow = 3)
+B <- matrix( c(11:19), nrow = 3)
 
 # crossproduct
-Not to be confused with a dot product (which is done with %*%). Regarding when to use ```crossprod()``` vs ```t(A) %*% B``` ... one may be faster than another based on the matrix being sparse. But this will only make a difference on massive matrices.
+# Not to be confused with a dot product (which is done with %*%). 
+# these are convenience functions. Can be duplicated with transpose & %*%
 
-```{r}
 crossprod(A,B)
-```
-...which is equivalent to...
-```{r}
+
+#...which is equivalent to...
 t(A) %*% B
-```
+
+# Regarding when to use crossprod() vs t(A) %*% B
+# one may be faster than another based on the matrix being sparse. 
+# But this will only make a difference on massive matrices.
 
 # tcrossproduct
-```{r}
+
 tcrossprod(A,B)
-```
-...which is equivalent to...
-```{r}
+
+#...which is equivalent to...
 A %*% t(B)
-```
+
 
