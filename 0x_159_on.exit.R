29914b39b69be68adfdc3bff7e270d76669f5479
Mark Niemann-Ross <mark@niemannross.com>
2020-07-28 16:28:16 -0700

    add examples

--- 0x_159_on.exit.R
+++ 0x_159_on.exit.R
@@ -1,1 +1,28 @@
 # on.exit()
+# use for reliable cleanup when exiting a function - even if the function crashes
+
+divideMe <- function(a,b) {
+  on.exit( expr = print("Do something Useful"))
+  print(a/b)
+  }
+
+divideMe(3,4)
+
+divideMe <- function(a,b) {
+  on.exit( expr = print("Do something Useful"))
+  print(a/b)
+  sys.on.exit() # what is queued up in on.exit?
+}
+
+divideMe <- function(a,b) {
+  on.exit( expr = print("Do something Useful"))
+  print(a/b)
+  on.exit(print("something more"), add = TRUE) # add something
+}
+
+divideMe <- function(a,b) {
+  on.exit( expr = print("Do something Useful"))
+  print(a/b)
+  on.exit() # reset
+}
+
