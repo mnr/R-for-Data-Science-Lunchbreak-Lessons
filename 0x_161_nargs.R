9938565bef68e2d30802143c781a6eade9500cee
Mark Niemann-Ross <mark@niemannross.com>
2020-07-28 21:02:47 -0700

    adding explanatory code

--- 0x_161_nargs.R
+++ 0x_161_nargs.R
@@ -1,1 +1,19 @@
-# on.exit()
+# nargs()
+
+# consider int main (int argc, char *argv[] )
+# nargs is equivalent to argc
+# list(...) is equivalent to char *argv[] (only for ... arguments)
+
+# so...what good is nargs?
+
+# use it to determine how many arguments are being passed in to the function
+imaFunction <- function(dArg, ...) {
+  print(paste("number of arguments: ",nargs()))
+  dotArgs <- list(...) # misses dArg because that's not in the ...
+  print(dotArgs)
+}
+
+
+imaFunction(dArg = 3, eArg = 4, zArg = 5)
+imaFunction(dArg = 3, eArg = 4)
+imaFunction(dArg = 3)
