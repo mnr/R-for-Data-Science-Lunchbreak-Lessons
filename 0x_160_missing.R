699304c4af65e159a03676b7ebdf1d18701268fe
Mark Niemann-Ross <mark@niemannross.com>
2020-07-28 19:56:16 -0700

    Add code to demonstrate concept

--- 0x_160_missing.R
+++ 0x_160_missing.R
@@ -1,1 +1,38 @@
-# on.exit()
+# missing
+
+# here's a simple function
+imaFunction <- function(dArg, eArg, zArg) {
+  someVal <- dArg + eArg + zArg
+  return(someVal)
+}
+
+imaFunction(dArg = 3, eArg = 4, zArg = 5)
+
+# what happens if an argument is missing?
+imaFunction(dArg = 3, eArg = 4)
+
+# if it's just a simple missing value, you can set a default value
+imaFunction <- function(dArg, eArg, zArg = 5) {
+  someVal <- dArg + eArg + zArg
+  return(someVal)
+}
+
+#...but what if a missing value requires sophisticated handling?
+imaFunction <- function(dArg, eArg, zArg) {
+  if (missing(zArg)) {
+    # ... some special way of obtaining a value
+    zArg <- as.integer(readline(prompt = "what value for zArg? "))
+  }
+  someVal <- dArg + eArg + zArg
+  return(someVal)
+}
+
+# or change the behavior of the function entirely
+imaFunction <- function(dArg, eArg, zArg) {
+  if (missing(zArg)) {
+    someVal <- dArg ^ eArg
+  } else {
+    someVal <- dArg + eArg + zArg
+  }
+  return(someVal)
+}
