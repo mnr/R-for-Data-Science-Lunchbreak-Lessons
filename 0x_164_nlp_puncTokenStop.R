2549a1ab85373ac60f81e7344c6bbbb1021131ad
Mark Niemann-Ross <mark@niemannross.com>
2020-08-07 16:12:22 -0700

    load gutenbergr. Attempt gsub

--- 0x_164_nlp_puncTokenStop.R
+++ 0x_164_nlp_puncTokenStop.R
@@ -1,1 +1,16 @@
 # NLP: remove punctuation, tokenization, remove stop words
+
+install.packages("tidyr")
+install.packages("gutenbergr")
+
+library(tidyr)
+library(gutenbergr)
+
+RT_works <- gutenberg_works(author == "Tagore, Rabindranath")
+RT_corpus <- gutenberg_download(RT_works$gutenberg_id, 
+                                strip = TRUE,
+                                meta_fields = c("title", "language"))
+
+# remove punctuation
+gsub("[[punct:]]", "", RT_corpus) # but this will take FOREVER
+
