fb1c387544d9f02995e60ba10d729d76c735e9fc
Mark Niemann-Ross <mark@niemannross.com>
2020-08-07 18:49:28 -0700

    implement tm. Is it correct?

--- 0x_164_nlp_puncTokenStop.R
+++ 0x_164_nlp_puncTokenStop.R
@@ -1,5 +1,7 @@
 # NLP: remove punctuation, tokenization, remove stop words
 
+
+# Download the works of Rabindranath Tagore -------------------------------
 install.packages("tidyr")
 install.packages("gutenbergr")
 
@@ -7,10 +9,26 @@ library(tidyr)
 library(gutenbergr)
 
 RT_works <- gutenberg_works(author == "Tagore, Rabindranath")
-RT_corpus <- gutenberg_download(RT_works$gutenberg_id, 
-                                strip = TRUE,
-                                meta_fields = c("title", "language"))
 
-# remove punctuation
-gsub("[[punct:]]", "", RT_corpus) # but this will take FOREVER
+downloadHere <- "WorksOfRabindranath"
+dir.create(downloadHere)
 
+for (dwnldIndex in 1:nrow(RT_works)) {
+  print(RT_works[dwnldIndex, ])
+  oneDF <- gutenberg_download(RT_works[dwnldIndex,"gutenberg_id"])
+  
+  saveHere <- file.path(downloadHere, make.names(RT_works[dwnldIndex,"title"]))
+  write.table(oneDF, saveHere)
+}
+
+# remove punctuation ------------------------
+## gsub("[[punct:]]", "", RT_corpus) # but this will take FOREVER
+
+# instead, use the tm package ------------------------
+install.packages("tm")
+library(tm)
+
+RT_corpus <- SimpleCorpus(DirSource(downloadHere))
+RT_corpus <- tm_map(RT_corpus, removePunctuation) # remove punctuation
+RT_corpus <- tm_map(RT_corpus, removeWords, stopwords("english") )# remove stopwords
+RT_corpus_token <- Boost_tokenizer(RT_corpus)
