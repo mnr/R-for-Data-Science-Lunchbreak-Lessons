509598848e8e8ed81d64bb3e8527242b600dd756
Mark Niemann-Ross <mark@niemannross.com>
2020-08-09 18:17:03 -0700

    use previous session results

--- 0x_165_nlp_stemLemma.R
+++ 0x_165_nlp_stemLemma.R
@@ -1,1 +1,8 @@
 # NLP: stemming and lemmatizing
+
+# start with RT_corpus and RT_corpus_token from previous session
+
+# Stemming with tm package ---------------------------------
+install.packages("tm")
+library(tm)
+
