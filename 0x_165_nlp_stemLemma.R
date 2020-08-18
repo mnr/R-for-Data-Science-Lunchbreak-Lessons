b55d0b24d528db4a7ae2409b1fa80a337db8b8c7
Mark Niemann-Ross <mark@niemannross.com>
2020-08-18 10:42:23 -0700

    add comments

--- 0x_165_nlp_stemLemma.R
+++ 0x_165_nlp_stemLemma.R
@@ -1,1 +1,7 @@
 # NLP: stemming and lemmatizing
+
+# start with the corpus developed in previous session
+# RT_corpus contains the work of Rabindranath Tagore as found at project gutenberg
+# RT_corpus_token is tokenized version of corpus
+
+# 
