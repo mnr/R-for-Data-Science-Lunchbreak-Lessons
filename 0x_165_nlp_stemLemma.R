f70c4cd4f7c9ff9d3f426fe29a68cf4a80dea32e
Mark Niemann-Ross <mark@niemannross.com>
2020-08-18 15:30:17 -0700

    adding explorations

--- 0x_165_nlp_stemLemma.R
+++ 0x_165_nlp_stemLemma.R
@@ -1,12 +1,34 @@
-# NLP: stemming and lemmatizing
+# NLP: create document vs terms. Analyze result.
+
+# Load the tm package ---------------------------------
+install.packages("tm")
+library(tm)
 
 # start with the corpus developed in previous session
 # RT_corpus contains the work of Rabindranath Tagore as found at project gutenberg
-# RT_corpus_token is tokenized version of corpus
+load(file = "Rabindranath_corpus.rdata")
 
-# start with RT_corpus and RT_corpus_token from previous session
 
-# Stemming with tm package ---------------------------------
-install.packages("tm")
-library(tm)
+# Term Document Matrix ----------------------------------------------------
+# Term Document is terms on rows.
+# Document Term is documents on rows
+RT_TDmatrix <- TermDocumentMatrix(RT_corpus)
+# this tokenizes and places terms across top. Inspect dimnames of RT_TDmatrix
+
+RT_DTmatrix <- DocumentTermMatrix(RT_corpus, control = list(stopwords = TRUE, 
+                                                            removePunctuation = TRUE,
+                                                            removeNumbers = TRUE,
+                                                            stemming = TRUE))
+inspect(RT_DTmatrix)
+
+# Analysis ----------------------------------------------------------------
+findFreqTerms(RT_DTmatrix, 1000)
+findMostFreqTerms(RT_DTmatrix, n = 10) # find most frequent terms in each document
+
+# only find certain terms from a dictionary
+inspect(DocumentTermMatrix(RT_corpus, control = list(stopwords = TRUE, 
+                                             removePunctuation = TRUE,
+                                             removeNumbers = TRUE,
+                                             stemming = TRUE,
+                                             dictionary = c("eye", "sad"))))
 
