3fef46e480c1f8699cb2f77cdf4d50359b65548a
Mark Niemann-Ross <mark@niemannross.com>
2020-08-20 16:21:18 -0700

    very basic overview of nlp with tidytext

--- 0x_168_nlp_tidytext.R
+++ 0x_168_nlp_tidytext.R
@@ -1,6 +1,31 @@
 # NLP packages - tidytext
 
-# https://www.tidytextmining.com/
+
+# Part of the tidyverse
+# one token per row, instead of Document-Term or Term-Document
+
 
 install.packages("tidytext")
 library(tidytext)
+library(dplyr)
+
+# We need a document. How about Rabindranath Tagore - Stray Birds
+RT_strayBirds <- gutenberg_download(6524, 
+                                strip = TRUE,
+                                meta_fields = c("title", "language", "author"))
+
+# Here's a tidytext process
+# 1) download
+# 2) unnest_tokens produces one token/word per line
+# 3) anti_join (from dplyr) removes stop words (stop_words provided by tidytext)
+# 4) count = count the token frequency and sort
+RT_strayBird <- gutenberg_download(6524, 
+                   strip = TRUE,
+                   meta_fields = c("title", "language", "author")) %>%
+  unnest_tokens(words, text) %>%
+  anti_join(stop_words, by = c("words" = "word")) %>%
+  count(words, sort = TRUE)
+
+# LOTS MORE...
+# Here's THE BOOK by Julia Silge and David Robinson
+# https://www.tidytextmining.com/
