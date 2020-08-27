ac30f442dd24673daa463e62eff14f7b8cacd281
Mark Niemann-Ross <mark@niemannross.com>
2020-08-27 11:41:27 -0700

    add categories

--- 0x_171_rtools.R
+++ 0x_171_rtools.R
@@ -6,9 +6,7 @@
 # install.packages("installr")
 library(installr)
 
-
-
-# there are ## categories in installr
+# there are roughly 4 categories in installr
 
 # install.etc -------------------------------------------------------------
 installr() # gui
@@ -16,21 +14,28 @@ install.7zip()
 install.git()
 install.github()
 install.java()
-install.R()
+install.R() # and updateR()
 install.nodejs()
 
 
 # Programming functions ---------------------------------------------------
-is.integer(3)
-str(3)
-installr:::check.integer(3)
-is.integer(3L)
+is.integer(3) # why is this false?
+str(3) # because 3 is stored as num
+installr:::check.integer(3) # this is expected
+
 
 # System utilities --------------------------------------------------------
 is.windows() # which is odd, seeing how installr only runs under windows
+is.x64()
 myip()
 # os.hibernate() - os commands from terminal
+up_folder("this/that/whereami")
+system.PATH() # os search path for exe
 
 
 # Package trivia ----------------------------------------------------------
 package_authors("base")
+
+
+# more at https://github.com/talgalili/installr
+# doc at https://talgalili.github.io/installr/reference
