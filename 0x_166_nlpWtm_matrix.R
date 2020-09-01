6a71fcb2c6250c0b5401bdc598d22622cade7423
Mark Niemann-Ross <mark@niemannross.com>
2020-09-01 10:33:47 -0700

    split 166

--- 0x_166_nlpWtm_matrix.R
+++ 0x_166_nlpWtm_matrix.R
@@ -1,7 +1,7 @@
-# NLP: create document vs terms. Analyze result.
+# NLP: create document vs terms. 
 
 # Load the tm package ---------------------------------
-install.packages("tm")
+# install.packages("tm")
 library(tm)
 
 # start with the corpus developed in previous session
@@ -29,38 +29,4 @@ inspect(RT_DTmatrix)
 inspect(RT_DTmatrix[ 3 , "heart"]) # row 3, column "heart"
 inspect(RT_DTmatrix[  , "heart"]) # all rows, column "heart"
 inspect(RT_DTmatrix[  , c("heart", "eye", "sad")]) # all rows, multiple columns
-
-
-# Analysis ----------------------------------------------------------------
-findFreqTerms(RT_DTmatrix, 1000) # find terms that appear > 1000 times
-findMostFreqTerms(RT_DTmatrix, n = 10) # find most frequent terms in each document
-
-# find associated words. Simple - this is not "sentiment"
-# note use of term-document, NOT document-term
-findAssocs(RT_TDmatrix, c("happi","sad"), c(.8, .7))
-
-# plot relationships
-# requires Rgraphviz from bioconductor
-install.packages("BiocManager")
-BiocManager::install("Rgraphviz")
-
-# then plot
-plot(RT_TDmatrix, 
-     terms = sample(Terms(RT_TDmatrix), 20),
-     corThreshold = .8,
-     weighting = TRUE)
-
-# get the most frequent terms of The Fugitive
-RT_frequent <- findMostFreqTerms(RT_DTmatrix, n = 15)
-names(RT_frequent$The.Fugitive)
-
-# Is there correlation between those terms?
-plot(RT_TDmatrix, 
-     terms = names(RT_frequent$The.Fugitive),
-     corThreshold = .75,
-     weighting = TRUE)
-
-
-# be sure to check out the Natural Language Processing task
-# https://cran.r-project.org/web/views/NaturalLanguageProcessing.html
 
