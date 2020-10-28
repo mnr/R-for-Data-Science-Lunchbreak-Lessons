b23088b5bfe38972b61915833dd432b502f236bd
Mark Niemann-Ross <mark@niemannross.com>
2020-10-27 21:58:31 -0700

    showing sparseMatrix

--- 0x_174_matrixPackage_sparseMatrix.R
+++ 0x_174_matrixPackage_sparseMatrix.R
@@ -4,4 +4,25 @@
 library(Matrix)
 
 # an alternative to Matrix(x, sparse = TRUE)
-sparseMatrix()
+
+matrixRows <- 10
+matrixCols <- 10
+
+valueInRow <- c(3, 6)
+valueInCol <- c(5, 2)
+
+newSparseMatrix <- sparseMatrix(i = valueInRow,
+                                j = valueInCol,
+                                dims = c(matrixRows, matrixCols)
+             )
+
+newSparseMatrix
+
+newSparseMatrix <- sparseMatrix(i = valueInRow,
+                                j = valueInCol,
+                                dims = c(matrixRows, matrixCols),
+                                x = c(30,62)
+)
+
+newSparseMatrix
+
