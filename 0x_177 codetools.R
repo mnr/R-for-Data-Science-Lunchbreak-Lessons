0b5a8c7a8ab905c04a0bee2cd1c9aa9e86575c28
Mark Niemann-Ross <mark@niemannross.com>
2020-11-07 01:15:30 -0800

    enhancing illustrations

--- 0x_177 codetools.R
+++ 0x_177 codetools.R
@@ -3,13 +3,14 @@
 library(codetools)
 
 
-badFunction <- function() {
+badFunction <- function(aye = 3, bee) {
+  bee = 5 # formal variable assignment
   blerb = 3 # defined, but never used
   athing * 2 # "athing" never defined
   notAfunc(23) # "notAfunc" never defined
 }
 
-checkUsage(badFunction)
+checkUsage(badFunction, all = TRUE)
 
 checkUsagePackage("base")
 
