65ad0ade4020c76de65e335e5b2e3896710e1e79
Mark Niemann-Ross <mark@niemannross.com>
2020-12-01 10:30:58 -0800

    fix the icon

--- 01_179 R on RPi.R
+++ 01_179 R on RPi.R
@@ -3,3 +3,9 @@
 # on a terminal
 # sudo apt-get update
 # sudo apt-get install r-base r-base-core r-base-dev
+
+# fix the menu
+# R appears in the Graphic Menu. To move it to the programming menu
+# RPi menu -> Preferences -> main menu editor
+# programming -> new item: Name=R, Command=R, Launch in terminal. OK
+# Fix Icon: Rpi menu -> right click on R. Choose Properties. Click on Icon to bring up chooser
