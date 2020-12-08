4be2abce850916bef8b108695870795aea4d77ca
Mark Niemann-Ross <mark@niemannross.com>
2020-12-07 17:13:23 -0800

    Now with sample code

--- 0x_179 R on RPi.R
+++ 0x_179 R on RPi.R
@@ -29,5 +29,51 @@
 # Geany terminal can be used to run R
 # also install packages
 # install.packages("cowsay")
+
 library(cowsay)
-say("Hello world")
+
+# say("Hello world")
+
+# plot(x = 1:10, y = 10:1)
+
+# controlling the GPIO
+hellobob <- system("pwd", intern=TRUE)
+print(paste("here i am", hellobob))
+
+doExampleOne <- TRUE
+#### Example 1 #################################
+# assign buttons
+btnAlpha <- 15 # BCM numbering scheme
+btnGamma <- 14
+
+
+while (doExampleOne) {
+  getGPIOcmd <- paste("gpioget gpiochip0", btnAlpha)
+  if (system(getGPIOcmd, intern = TRUE) == 0) print("Alpha")
+  getGPIOcmd <- paste("gpioget gpiochip0", btnGamma)
+  if (system(getGPIOcmd, intern = TRUE) == 0) print("Gamma")
+  
+}
+# control-Z stops the loop
+
+#### Example 2 ##################################
+# assign buttons as a vector
+allBtns <- c(15,14)
+
+while (!doExampleOne) {
+  getGPIOcmd <- paste("gpioget --active-low gpiochip0 ", paste(allBtns, collapse= " "))
+  #whatIsThis <- system(getGPIOcmd, intern = TRUE)
+  #whatIsThis
+  pinState <- system(getGPIOcmd, intern = TRUE)
+  gpioPins <- as.integer(unlist(strsplit(pinState, " ")))
+  if (any(gpioPins)) { 
+    print(paste("a=",gpioPins[1]," b=",gpioPins[2]))
+  }
+  if (all(gpioPins)) {
+    print("all buttons are pressed")
+  }
+  if (!any(gpioPins)) {
+    print("no buttons are pressed")
+  }
+  
+}
