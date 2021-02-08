f75c1a9a13b38d29b4dc0e94da8807787da29c1b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-02-08 00:05:09 -0800

    research

--- 0x_181 Run R on a schedule.R
+++ 0x_181 Run R on a schedule.R
@@ -1,5 +1,11 @@
 # run an R script on a schedule
 
 # Windows ?
+https://cran.r-project.org/package=taskscheduleR
 
 # Macintosh & Linux: Cron
+https://cran.r-project.org/package=cronR
+
+
+http://theautomatic.net/2020/05/12/how-to-schedule-r-scripts
+http://theautomatic.net/2018/10/31/how-to-run-r-from-the-task-scheduler/
