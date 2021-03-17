33edf2fdc1c8b0db71f5eb3d0cdaf09947e12a69
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-03-17 12:49:40 -0700

    updating mona

--- 0x_189 clustering with mona.R
+++ 0x_189 clustering with mona.R
@@ -2,31 +2,37 @@
 # Hierarchical method
 # MONothetic analysis clustering of binary variables
 # similar to divisive - starts with one cluster
+# used for binary datasets
 
 # Titanic has three binary attributes
 
-# convert from array to data.frame
+# convert titanic dataset from array to data.frame
 #https://stackoverflow.com/questions/59447399/how-to-transform-the-titanic-data-set
 d1 <- as.data.frame(Titanic)
 Tita <- d1[rep(seq_len(nrow(d1)), d1$Freq),1:4]
 row.names(Tita) <- NULL
 
 str(Tita)
-# sex, age, and survived are binary
+# sex, age, and survived are binary/factors
 
-Tita$Survived
-
+# downsample for this example
 smallTitanic <- Tita[sample(nrow(Tita), 30),
                      c("Sex","Age","Survived")]
 
 
 library(cluster)
 monaTitanic <- mona(smallTitanic)
-plot(monaTitanic)
 monaTitanic
 
+plot(monaTitanic) # only bannerplot is available
+
+# examine mona object
 data.frame("order"=monaTitanic$order,
            "Row Number"=monaTitanic$order.lab,
            "Split Variable"=c(monaTitanic$variable,"NULL"),
            "Step Level"=c(monaTitanic$step,"0")
 )
+
+# summary
+# mona is divisive hclust for binary datasets
+# plotting isn't well supported
