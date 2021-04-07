508a912409ff14069589defd38f9a04326025b6b
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-04-06 17:15:50 -0700

    renumber and record

--- 01_188 cutree with hclust.R
+++ 01_188 cutree with hclust.R
@@ -6,13 +6,15 @@ library(cluster)
 # downsample quakes for demonstration. from 500 to 25
 simpleQuakes <- quakes[sample(nrow(quakes),25),c("long","lat")]
 
-cutree(hclust(dist(sampleQuakes)),k=2)
+plot(hclust(dist(simpleQuakes)))
+     
+# cluster by k
+cutree(hclust(dist(simpleQuakes)),k=2)
+
+# cluster by height
+cutree(hclust(dist(simpleQuakes)),h=15)
+
 
 # agnes produces hclust objects, so hclust functions work
 cutree(agnes(simpleQuakes), k=2)
 
-# interactive cluster identification
-plot(hclust(dist(sampleQuakes)),
-     labels = c(LETTERS[1:length(sampleQuakes)])) # labeled points
-returnedPoints <- identify(hclust(dist(sampleQuakes)), n=2, MAXCLUSTER = 3)
-returnedPoints
