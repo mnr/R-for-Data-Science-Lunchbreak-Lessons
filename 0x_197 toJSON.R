58881b86232955d625d5d0e05fd7d5a457eda393
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-06-04 08:40:25 -0700

    initial commit

--- 0x_197 toJSON.R
+++ 0x_197 toJSON.R
@@ -4,5 +4,13 @@
 #install.packages("jsonlite")
 library(jsonlite)
 
+# convert object to JSON string
+toJSON(ChickWeight)
+toJSON(ChickWeight, dataframe = "values") # vs rows. Or columns
+toJSON(ChickWeight, pretty = TRUE) # nice formatting
+toJSON(ChickWeight, pretty = TRUE, dataframe = "rows") # nice formatting
+
+
+# write json to a file on disk
 cwjson <- toJSON(ChickWeight)
 write_json(cwjson, path = "chickWeight.json")
