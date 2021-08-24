6168ecc81cce55df31c05035682aa27e85c83d61
Mark Niemann-Ross <mark@niemannross.com>
2021-08-24 02:54:02 -0700

    new text

--- 0x_204 installpackages.R
+++ 0x_204 installpackages.R
@@ -1,1 +1,14 @@
 #install.packages
+
+# pkgs is optional, but must be a character vector
+install.packages(cowsay) # doesn't work
+install.packages("cowsay")
+#...or...
+installThese <- c("cowsay")
+install.packages(installThese)
+# so can install multiple packages
+installThese <- c("cowsay", "fun")
+
+install.packages() # missing "pkgs=" brings up a list
+
+install.packages(pkgs="cowsay", quiet = TRUE) # turns off progress report
