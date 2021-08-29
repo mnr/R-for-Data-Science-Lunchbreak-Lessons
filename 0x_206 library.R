8b7a25096981cec62a72debf3e2c9405691b6866
Mark Niemann-Ross <mark@niemannross.com>
2021-08-29 08:55:27 -0700

    initial commit

--- 0x_206 library.R
+++ 0x_206 library.R
@@ -1,1 +1,18 @@
-# library()
+# library(), require(), detach()
+
+# setup for error
+remove.packages("cowsay")
+
+library(fun)
+library(fun, verbose = TRUE)
+library(help = "fun") # documentation for a package
+library(cowsay, logical.return = TRUE) # returns error if not installed
+library() #available libraries
+
+# check for FOSS
+getOption("checkPackageLicense") # if true, interactive check for FOSS
+
+require(fun) # use in a function. Returns a logical listing success
+require(cowsay) # same as logical.return=true
+
+detach() # unloads a package. Used to reload a package. most likely during development
