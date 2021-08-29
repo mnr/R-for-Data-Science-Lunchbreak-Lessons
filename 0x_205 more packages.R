d996b927bc5afe45e43f737501b5a173965a677f
Mark Niemann-Ross <mark@niemannross.com>
2021-08-29 04:18:33 -0700

    initial commit

--- 0x_205 more packages.R
+++ 0x_205 more packages.R
@@ -1,2 +1,15 @@
 # old.packages, new.packages, and update.packages
 # available.packages, contrib.url, remove.packages
+
+# the most important tool of this set
+update.packages() # offers to update old packages
+
+# install.packages has several related commands
+old.packages() # shows old packages in your system
+new.packages() # lists packages you don't have
+
+# lists information about packages you could have FOR THIS SYSTEM (version and OS)
+available.packages() 
+available.packages(filters = "OS_type")
+
+remove.packages("cowsay") # does what you expect
