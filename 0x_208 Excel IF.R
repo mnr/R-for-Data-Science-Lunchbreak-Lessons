1d06a7338d236e002853e9da7aefdc184ac28bc3
Mark Niemann-Ross <mark@niemannross.com>
2021-10-22 15:31:31 -0700

    initial commit

--- 0x_208 Excel IF.R
+++ 0x_208 Excel IF.R
@@ -1,1 +1,28 @@
-# library()
+# The IF function allows you to make logical comparisons 
+# between a value and what you expect.
+
+# R equivalent is if...then...else
+library(readxl)
+
+Spreadsheet <- as.data.frame(read_excel("SampleSpreadsheet.xls",  col_names = FALSE))
+
+#=IF(A1>1,"This is greater than one")
+if(Spreadsheet[1,1] > 1) { 
+  print("This is greater than one")
+  }
+  
+# =IF(A1>1,"This is greater than one","This is less than or equal to one")
+if(Spreadsheet[1,1] > 1) { 
+  print("This is greater than one")
+} else {print("This is less than or equal to one")}
+# hint - Spreadsheet [3,6] == 1
+
+ltgt1 <- function(rowOfValues) {
+  for (aValue in rowOfValues) {
+  if(aValue > 1) { 
+    print(paste(aValue,"is greater than one"))
+  } else {print(paste(aValue," is less than or equal to one"))}
+  }}
+
+# equivalent of if() for each cell in spreadsheet
+apply(Spreadsheet, MARGIN = 2 ,FUN=ltgt1 )
