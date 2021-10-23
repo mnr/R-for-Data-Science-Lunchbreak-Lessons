089828893931b95f5a5fe475cc3952df171c8df2
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-10-22 23:50:52 -0700

    initial commit

--- 0x_216 Excel INDEX.R
+++ 0x_216 Excel INDEX.R
@@ -1,1 +1,10 @@
-# library()
+# index returns a value or the reference to a value from within a table or range.
+
+# =INDEX(A1:F6,3,2) is 6
+
+library(readxl)
+
+Spreadsheet <- as.data.frame(read_excel("SampleSpreadsheet.xls",  col_names = FALSE, sheet = "numbers"))
+
+# R is exceedingly good at this...
+Spreadsheet[3,2]
