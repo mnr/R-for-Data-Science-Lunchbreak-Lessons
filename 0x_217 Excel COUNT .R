f5993212b6a04a9004ed7a5b6369c83b6c4e7eea
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-10-23 00:16:08 -0700

    initial commit

--- 0x_217 Excel COUNT .R
+++ 0x_217 Excel COUNT .R
@@ -1,1 +1,13 @@
-# library()
+# count - the number of cells that contain numbers
+
+# =COUNT(A1:F6) = 34 (because of two NA)
+
+library(readxl)
+
+# as.matrix ensures this is all one type - in this case, numeric
+Spreadsheet <- as.matrix(read_excel("SampleSpreadsheet.xls",  col_names = FALSE, sheet = "numbers"))
+
+length(Spreadsheet) # = 36 - but includes NA
+is.na(Spreadsheet) #returns true/false on NA
+!is.na(Spreadsheet) # inverts T/F
+sum(!is.na(Spreadsheet)) # TRUE is = 1. sum(True, True, False) = 2
