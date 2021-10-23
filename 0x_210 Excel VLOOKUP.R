db5c4f60abf6201212a830adddcf4b0a1389e277
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-10-22 21:16:59 -0700

    Initial commit

--- 0x_210 Excel VLOOKUP.R
+++ 0x_210 Excel VLOOKUP.R
@@ -1,1 +1,14 @@
-# library()
+# VLOOKUP - find things in a table or a range by row. 
+# for example... =VLOOKUP(5,A1:B6,2) = 8
+
+library(readxl)
+
+Spreadsheet <- as.data.frame(read_excel("SampleSpreadsheet.xls",  col_names = FALSE))
+
+# vlookup is easy in R
+Spreadsheet[Spreadsheet[,1] == 5,2]
+
+# break it down...
+Spreadsheet[ ,1] # is the first column
+Spreadsheet[ ,1] == 5 # which values in column 1 are equal to 5?
+Spreadsheet[ Spreadsheet[ ,1] == 5, 2 ] # return the value of column 2 where row 1 is equal to five
