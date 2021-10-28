0617b8948aa22fe91921e0d248523ce6e5cc0c3c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2021-10-28 15:20:19 -0700

    initial commit

--- 0x_225 Excel LEN.R
+++ 0x_225 Excel LEN.R
@@ -1,10 +1,18 @@
-# len()
+# =len() returns the number of characters in a text string.
+
+
 
 library(readxl)
 
-Spreadsheet <- as.data.frame(read_excel("SampleSpreadsheet.xls",  col_names = FALSE, sheet = "numbers"))
+Spreadsheet <- as.data.frame(read_excel("SampleSpreadsheet.xls",  
+                                        col_names = FALSE, 
+                                        sheet = "text"))
 
+# =LEN(A1) is 4
+nchar(Spreadsheet[1,1]) # = 4
 
+# you might think of the length() R function, but...
+length(Spreadsheet[1,1]) # = 1 because length() counts elements
 
 # or look at excelfunctionsR
 # https://cran.r-project.org/package=ExcelFunctionsR
