0b766315d1b8f8bd59248de39fd790c1fbaead7d
Mark Niemann-Ross <mark@niemannross.com>
2021-11-02 12:02:47 -0700

    record and renumber

--- 01_219 Excel SUMIF AVERAGEIF.R
+++ 01_219 Excel SUMIF AVERAGEIF.R
@@ -6,9 +6,9 @@ library(readxl)
 
 Spreadsheet <- as.matrix(read_excel("SampleSpreadsheet.xls",  col_names = FALSE, sheet = "numbers"))
 
-# =AVERAGEIF(A1:F1,">3") average of 1st row where numbers > 5
+# =AVERAGEIF(A1:F1,">3") average of 1st row where numbers > 3
 Spreadsheet[1,] # the first row of the spreadsheet
-Spreadsheet[1,] >5 # produces True/False
+Spreadsheet[1,] >3 # produces True/False
 Spreadsheet[1,Spreadsheet[1,]>3] # produces numbers in 1st row greater than three
 
 # then averageif and sumif
@@ -20,5 +20,5 @@ sum(Spreadsheet[1,Spreadsheet[1,]>3])
 # install.packages("ExcelFunctionsR")
 library(ExcelFunctionsR)
 
-AVERAGEIF(Spreadsheet[1,], ">3") # doesn't understand operators
-AVERAGEIF(Spreadsheet[1,], "3") # insists on average range
+AVERAGEIF(Spreadsheet[1,], ">3", Spreadsheet[1,]) # insists on average range
+SUMIF(Spreadsheet[1,], ">3", Spreadsheet[1,]) 
