735c37886986a520b2e5274053a83841032905f9
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-02-15 15:26:41 -0600

    initial commit

--- 0x_230 vectorize.R
+++ 0x_230 vectorize.R
@@ -1,1 +1,27 @@
-Vectorize()
+# Vectorize a function that normally isn't vectorized
+# note capital V
+
+# start with a sample function
+myIf <- function(myVarA, myVarB) {
+  if (myVarA == myVarB) {
+    return(paste(myVarA, myVarB, "They Match"))
+  } else {
+    return(paste(myVarA, myVarB, "They Don't Match"))
+  }
+}
+
+# Works as long as arguments are single numbers
+myIf(3,4)
+myIf(1,1)
+
+# But fails if arguments are vectors
+myIf(c(1:3), c(1,5,7))
+
+# solution - Vectorize()
+myIfVect <- Vectorize(myIf)
+
+# now it works
+myIfVect(c(1:3), c(1,5,7))
+
+# results are identical to mapply
+mapply(myIf, c(1:3), c(1,5,7))
