40f9188a22f1333829a787c34ff47328af8d1832
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-02-21 14:19:54 -0600

    Notes for presentation

--- 0x_231 Powerpoint from R.R
+++ 0x_231 Powerpoint from R.R
@@ -1,1 +1,18 @@
 # building powerpoint with R
+
+# Use Rmarkdown
+
+# rstudio:file:new file: r markdown...
+# ...then...
+# choose presentation:powerpoint
+
+# dynamic date - include this in header
+date: "`r Sys.Date()`"
+
+
+https://bookdown.org/yihui/rmarkdown/powerpoint-presentation.html
+https://pandoc.org/MANUAL.html#slide-shows
+https://github.com/jgm/pandoc/blob/master/test/pptx/reference-depth.pptx
+
+# html options often don't work. For example: df_print = paged fails.
+# instead for tables, use kable as illustrated in example
