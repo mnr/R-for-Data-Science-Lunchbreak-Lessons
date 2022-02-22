f83b06febd26e0061a014d3d881a5805bd844e11
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-02-21 22:05:18 -0600

    updates and details

--- 0x_234 flowcharting.R
+++ 0x_234 flowcharting.R
@@ -3,5 +3,27 @@
 # install.packages("flow")
 library(flow)
 
+stuffToDo <- function(thisThing, thatThing) {
+  if (thisThing > thatThing) {
+    print(thatThing)
+  } else {
+    print(thisThing)
+  }
+  looper <- 10
+  while (looper != 0) {
+    looper <- looper - 1
+  }
+  bobaloo <- thisThing + thatThing
+}
+
+# static display of function logic
+flow_view(stuffToDo)
+flow_view(stuffToDo, truncate = 20)
+
+# dynamic display of code during execution
+flow_run(stuffToDo(3,5))
+flow_run(stuffToDo(10,9))
+
+# can be done with source files
 flow_view("../R_code_challenge/04_01 rock paper scissors lizard spock SOLUTION.R")
 
