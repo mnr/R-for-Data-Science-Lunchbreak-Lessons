f0a3a1f6c835e7a494c44103c9c5c3194b4f19cc
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-02-21 22:53:40 -0600

    save for later

---
+++ to_be_later/0x_235 sql in rstudio.sql
@@ -0,0 +1,7 @@
+-- using SQL in Rstudio
+/* first: open a connection
+      https://support.rstudio.com/hc/en-us/articles/115010915687-Using-RStudio-Connections-in-the-RStudio-IDE
+*/
+-- !preview conn=DBI::dbConnect(RSQLite::SQLite())
+
+SELECT 1
