c0ac47d8e135ccc93aaed228734a8ea1f8543552
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-03-01 15:51:31 -0600

    factorial is only part of Special math functions

--- 0x_241_factorial.R
+++ 0x_241_factorial.R
@@ -1,5 +1,23 @@
 # factorial
 
+# note: there are a selection of functions for
+# beta and gamma functions
+?Special
+
+beta(a, b)
+lbeta(a, b)
+
+gamma(x)
+lgamma(x)
+psigamma(x, deriv = 0)
+digamma(x)
+trigamma(x)
+
+choose(n, k)
+lchoose(n, k)
+factorial(x)
+lfactorial(x)
+
 n <- 3 # for an n x n matrix
 factorial(n) # provides the number of permutations
 plot(factorial(1:5), type = "l")
