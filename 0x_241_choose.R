d2fbe963d7c572a37ccde39c668c452160e96ef3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-03-02 09:59:17 -0700

    initial commit

---
+++ 0x_241_choose.R
@@ -0,0 +1,18 @@
+# choose
+
+# binomial coefficient
+# indexed by a pair of integers: n and k
+# n >= k >= 0
+# notated as $\binom{n}{k}$ 
+# pronounced "n choose k"
+
+choose(5,2)
+# combn() all possible combinations
+combn(5,2)
+
+# another example
+choose(6,3)
+combn(6,3)
+
+# lchoose is the log of choose (although it evaluates as false)
+lchoose(5,2) == log(choose(5,2))
