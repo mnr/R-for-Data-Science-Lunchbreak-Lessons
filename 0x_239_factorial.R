78dfbc904a00c1b6a4df1509d24640b285bc7d6c
Mark Niemann-Ross <mark@niemannross.com>
2022-03-14 17:40:10 -0700

    swap out send_mail for factorial

---
+++ 0x_239_factorial.R
@@ -0,0 +1,22 @@
+# factorial
+
+# factorial seven is written 7!
+# meaning 1 × 2 × 3 × 4 × 5 × 6 × 7
+
+?Special
+
+x <-  7
+
+factorial(x)
+gamma(x+1)
+
+# natural log
+lfactorial(x)
+lgamma(x+1)
+
+exp(lfactorial(x))
+exp(1) ^ lfactorial(x)
+
+
+plot(factorial(1:x), type = "l")
+plot(lfactorial(1:x), type = "l")
--- 0x_242_special functions.R
+++ 0x_242_special functions.R
@@ -13,12 +13,4 @@ psigamma(x, deriv = 0)
 digamma(x)
 trigamma(x)
 
-choose(n, k)
-lchoose(n, k)
-factorial(x)
-lfactorial(x)
 
-n <- 3 # for an n x n matrix
-factorial(n) # provides the number of permutations
-plot(factorial(1:5), type = "l")
-plot(gamma(1:5), type = "l")
