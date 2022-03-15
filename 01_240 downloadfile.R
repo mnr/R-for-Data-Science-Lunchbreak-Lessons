8de17876beadcaa9d1b5b9071b6b205f09fc6cb0
Mark Niemann-Ross <mark@niemannross.com>
2022-03-15 12:17:23 -0700

    updates

--- 01_240 downloadfile.R
+++ 01_240 downloadfile.R
@@ -1,10 +1,19 @@
 # download.file
-download.file(url, destfile, method, quiet = FALSE, mode = "w",
-              cacheOK = TRUE,
-              extra = getOption("download.file.extra"),
-              headers = NULL, ...)
 
+url <- "https://i0.wp.com/niemannross.com/wp-content/uploads/2018/08/cropped-mnr-in-japan-smaller.jpg"
+destfile <- "I_downloaded_this.jpg"
+
+download.file(url, 
+              destfile, 
+              quiet = FALSE, 
+              method = "auto",
+              mode = "w",
+              extra = getOption("download.file.extra")
+              )
 
 # see also curl::curl_download
-curl_download(url, destfile, quiet = TRUE, 
+library(curl)
+curl_download(url, destfile, quiet = FALSE, 
               mode = "wb", handle = new_handle())
+
+
