e919ed94d01213e4227b7c4445b0a194e8c0b593
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-04-07 18:06:59 -0700

    initial commit

---
+++ 0x_245_asPOSIXct.R
@@ -0,0 +1,16 @@
+# factorial
+
+# note: there are a selection of functions for
+# beta and gamma functions
+?Special
+
+beta(a, b)
+lbeta(a, b)
+
+gamma(x)
+lgamma(x)
+psigamma(x, deriv = 0)
+digamma(x)
+trigamma(x)
+
+
