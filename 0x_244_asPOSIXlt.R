52e322929f1feae4cd5cd5a7572ded25201bf1b8
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-04-11 16:10:05 -0700

    scripted

--- 0x_244_asPOSIXlt.R
+++ 0x_244_asPOSIXlt.R
@@ -1,16 +1,31 @@
-# factorial
+# as.POSIXlt
 
-# note: there are a selection of functions for
-# beta and gamma functions
-?Special
+# Date is only date.
+# POSIXlt and POSIXct are date & time
+# POSIXlt and POSIXct are related
+# POSIXlt stores date/time as components (Years, Days, Months, Hours...)
+# POSIXct stores date/time as seconds since 1970-01-01 00:00:00 UTC
 
-beta(a, b)
-lbeta(a, b)
+as.POSIXlt("2022-03-23")
+str(as.POSIXlt("2022-03-23"))
 
-gamma(x)
-lgamma(x)
-psigamma(x, deriv = 0)
-digamma(x)
-trigamma(x)
+myDateTime <- as.POSIXlt("2022-03-23")
+myDateTime <- as.POSIXlt("2022-03-23 11:23:14")
+myDateTime <- as.POSIXlt("2022-03-23 11:23:14", tz = "US/Pacific")
+# I'll talk more about timezones in a later session
 
+# retrieve parts of a date in POSIXlt format
+# Essentially, treat it like a list
+myDateTime$year + 1900 # add 1900 for current year
+?DateTimeClasses # for parts of POSIXlt
+myDateTime$mon
+myDateTime$mday
 
+# date time math
+myDateTime$year + 1900
+myDateTime$year <- myDateTime$year + 1
+myDateTime
+myDateTime$mday <- myDateTime$mday + 31
+myDateTime
+myDateTime$hour <- 25 # set to 25 hours
+myDateTime
