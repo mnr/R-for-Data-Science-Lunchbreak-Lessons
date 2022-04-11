fd57d08acba827c929692e05e18c55b61afdcaac
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-04-10 19:27:35 -0700

    prep examples

---
+++ 0x_242_beta_gamma.R
@@ -0,0 +1,30 @@
+# factorial
+
+# A selection of functions for beta and gamma functions
+
+# start with beta
+mmm <- 3
+nnn <- 4
+beta(mmm,nnn)
+beta(mmm,nnn) == factorial(mmm - 1) * factorial(nnn - 1) / factorial(mmm + nnn - 1)
+
+# gamma
+gamma(mmm)
+gamma(nnn)
+beta(mmm,nnn) == gamma(mmm) * gamma(nnn) / gamma(mmm + nnn)
+
+# natural logarithm of functions
+lbeta(a, b)
+lgamma(x)
+
+# first and second derivatives
+digamma(mmm)
+trigamma(mmm)
+
+# other derivatives
+psigamma(mmm, deriv = 0) 
+psigamma(mmm, deriv = 0 ) == digamma(mmm)
+psigamma(mmm, deriv = 1 ) == trigamma(mmm)
+
+# more details
+?Special
