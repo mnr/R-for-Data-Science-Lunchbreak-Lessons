4805d30a855f593c4cf7b6b0885c8c62575eee47
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-04-14 04:21:56 -0700

    scripted

--- 0x_248_systemTimezone.R
+++ 0x_248_systemTimezone.R
@@ -1,3 +1,23 @@
+# timezones and olsonnames
+
+# current system timezone
+Sys.timezone()
+# dependent on operating system
+# system preferences - date & time
+
+# time zone is stored in POSIXlt
+myPLTDateTime <- as.POSIXlt(Sys.time())
+unlist(myPLTDateTime)
+myPLTDateTime$zone
+myPLTDateTime$gmtoff  
+
+# time zone trivia
+Sys.getenv("TZDIR") # location of time zone databases
+
+OlsonNames()
+# database created by Arthur David Olson
+match(Sys.timezone(), OlsonNames())
+OlsonNames()[147]
 
 
 # for more on dates and times in R...
