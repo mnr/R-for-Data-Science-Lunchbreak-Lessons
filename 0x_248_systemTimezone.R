7805e6435dd52478c0448b2feef7607fde438a86
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-04-14 04:30:58 -0700

    changes in edits

--- 0x_248_systemTimezone.R
+++ 0x_248_systemTimezone.R
@@ -9,16 +9,20 @@ Sys.timezone()
 myPLTDateTime <- as.POSIXlt(Sys.time())
 unlist(myPLTDateTime)
 myPLTDateTime$zone
-myPLTDateTime$gmtoff  
-
-# time zone trivia
-Sys.getenv("TZDIR") # location of time zone databases
+myPLTDateTime$gmtoff
 
 OlsonNames()
 # database created by Arthur David Olson
 match(Sys.timezone(), OlsonNames())
 OlsonNames()[147]
 
+# change time zone when object is created
+myPLTDateTime <- as.POSIXlt(Sys.time(), tz = OlsonNames()[127])
+unlist(myPLTDateTime)
+myPLTDateTime$zone <- "PDT" #but that doesn't change the time, just TZ
+
+# time zone trivia
+Sys.getenv("TZDIR") # location of time zone databases
 
 # for more on dates and times in R...
 # https://linkedin-learning.pxf.io/r_dates
