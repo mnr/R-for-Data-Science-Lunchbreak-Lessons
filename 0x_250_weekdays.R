a1b251fe836dd08ecdceeff0b5418dc19f0c29b3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-04-15 11:15:01 -0700

    scripted

--- 0x_250_weekdays.R
+++ 0x_250_weekdays.R
@@ -1,4 +1,25 @@
 # weekdays, months, quarters, & julian
 
+mydate <- Sys.Date()
+
+weekdays(mydate)
+weekdays(mydate + 10) 
+weekdays(mydate + 1:5) # handles a vector of multiple dates
+weekdays(mydate + 1:5, abbreviate = TRUE)
+
+# likewise -------
+months(mydate)
+quarters(mydate)
+
+# julian is for days since 1970-01-01
+julian(mydate) 
+
+# change the origin to calculate days since ....
+julian(mydate, origin = as.Date("2000-01-01")) # days since Y2K
+
+# julian also works with datetime
+julian(as.POSIXct(Sys.time()))
+julian(as.POSIXlt(Sys.time()))
+
 # for more on dates and times in R...
 # https://linkedin-learning.pxf.io/r_dates
