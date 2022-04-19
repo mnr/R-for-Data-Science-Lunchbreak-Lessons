36fa76146171719b9e0a34cdb17f2548fa00d3d7
Mark Niemann-Ross <mark@niemannross.com>
2022-04-19 16:24:32 -0700

    record and renumber

---
+++ 01_253_SFplumber.R
@@ -0,0 +1,23 @@
+#
+# This is a Plumber API. You can run the API by clicking
+# the 'Run API' button above.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+
+#* @apiTitle Simple plumber exercise file
+#* @apiDescription Provided as part of LinkedIn Learning: R for Data Science, lunchbreak lessons
+
+# saved as 01_254_SFplumber.r
+#* simple manipulation of input
+#* @param this a string to UPPERCASE
+#* @param that a string to count
+#* @get /changeStrings
+simpleFunction <- function(this, that) {
+  paste(toupper(this), nchar(that) )
+}
+
