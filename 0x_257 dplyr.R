3484c4a8945ccdab3cc85d8b295fc69923c73ca7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-06-22 09:03:20 -0500

    finished example

--- 0x_257 dplyr.R
+++ 0x_257 dplyr.R
@@ -3,9 +3,21 @@
 # a package for data manipulation
 # part of the tidyverse
 
+# provides several functions
 # mutate() adds new variables that are functions of existing variables
 # select() picks variables based on their names.
 # filter() picks cases based on their values.
 # summarise() reduces multiple values down to a single summary.
 # arrange() changes the ordering of the rows.
 # These all combine naturally with group_by() which allows you to perform any operation “by group”.
+
+# for example...
+worldTemp <- read_csv("https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.csv", 
+                      skip = 1,
+                      col_types = "nnnnnnnnnnnnnnnnnnn",
+                      na = "***") %>% 
+  select(c("Year", month.abb)) %>% 
+  filter(Jan > 0) %>%
+  rowwise() %>% 
+  mutate(medianTemp = median(c_across())) %>% 
+  arrange(medianTemp)
