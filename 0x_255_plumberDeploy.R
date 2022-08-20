a1e2142db373e3e4971ede9b4b1449d0b6f136a7
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-08-20 01:21:04 -0700

    adds to documentation

--- 0x_255_plumberDeploy.R
+++ 0x_255_plumberDeploy.R
@@ -1,3 +1,15 @@
 # plumber deploy
 
 # https://www.rplumber.io/articles/hosting.html
+
+# Another way: rstudio server Open Source Edition
+# https://www.rstudio.com/products/workbench/comparison/
+
+# download the server
+# https://www.rstudio.com/products/rstudio/download-server/
+
+# accessing server
+# https://support.rstudio.com/hc/en-us/articles/200552306-Getting-Started
+
+# set up rpi
+# https://community.rstudio.com/t/setting-up-your-own-shiny-server-rstudio-server-on-a-raspberry-pi-3b/18982
