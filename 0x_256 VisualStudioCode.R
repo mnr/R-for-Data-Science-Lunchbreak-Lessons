dc8b9871a9cb8c909c398c8f532e91a20f59ea63
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-08-21 02:52:41 -0700

    adding notes

--- 0x_256 VisualStudioCode.R
+++ 0x_256 VisualStudioCode.R
@@ -1,6 +1,17 @@
-# how to configure visual studio code
+# how to configure visual studio code for R
+
+# learn visual studio code at linkedin learning
+# https://www.linkedin.com/learning/topics/visual-studio-code
 
 # download vs code from https://code.visualstudio.com/Download
 # install
 
 # use extensions to add R
+# search for R
+# R extension for visual studio code
+# R debugger
+
+# look for R logo in left side - SUCCESS!
+# maybe restart if features don't show up
+
+# more help - https://code.visualstudio.com/docs/languages/r
