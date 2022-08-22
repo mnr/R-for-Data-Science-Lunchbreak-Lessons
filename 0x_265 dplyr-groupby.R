1be4eaca35da14e515fa61b80ba76650fe005c8e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-08-22 11:49:45 -0700

    adding example

--- 0x_265 dplyr-groupby.R
+++ 0x_265 dplyr-groupby.R
@@ -1,3 +1,17 @@
-# https://dplyr.tidyverse.org
+# These all combine naturally with group_by() 
+# which allows you to perform any operation “by group”.
 
-# These all combine naturally with group_by() which allows you to perform any operation “by group”.
+# install.packages("tidyverse")
+library("tidyverse")
+
+# without any grouping
+read.csv("https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.csv", 
+                        skip = 1) %>%
+                summarize(median(Jan))
+
+# what if you want median for each century?
+read.csv("https://data.giss.nasa.gov/gistemp/tabledata_v4/GLB.Ts+dSST.csv", 
+                        skip = 1) %>%
+                group_by(trunc(Year/100)) %>%
+                summarize(median(Jan))
+
