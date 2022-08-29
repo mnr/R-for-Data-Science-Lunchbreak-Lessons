a008238b8aa347a2fcc14fa96b43790da092f650
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-08-29 15:27:20 -0700

    update notes

--- 0x_255 getwd setwd.R
+++ 0x_255 getwd setwd.R
@@ -1,12 +1,19 @@
 # getwd setwd
 
+# what is a working directory?
 
+# if you ask for a relative file, R will start in the working directory
+
+# fails because working directory is home director
+if (file.exists(".RData")) {print("this file exists")}
+# more...set working directory
+
+# how to check this
 getwd() # returns current working directory
-
-setwd("~") # tilde is shorthand for home directory
 
 basename(getwd())
 
 dirname(getwd())
 
-path.expand("~/foo")
+setwd("~") # tilde is shorthand for home directory
+
