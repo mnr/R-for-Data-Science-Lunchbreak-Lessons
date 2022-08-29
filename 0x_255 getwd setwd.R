a7349d1b9e66858f7699adb85eef47babfbc1842
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-08-29 15:17:39 -0700

    move getwd up to replace plumber deploy

---
+++ 0x_255 getwd setwd.R
@@ -0,0 +1,12 @@
+# getwd setwd
+
+
+getwd() # returns current working directory
+
+setwd("~") # tilde is shorthand for home directory
+
+basename(getwd())
+
+dirname(getwd())
+
+path.expand("~/foo")
