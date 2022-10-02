8db108a164e4f69abe4e022503483a5b18b140e2
Mark Niemann-Ross <mark.niemannross@gmail.com>
2022-10-02 11:17:03 -0500

    working on example

--- 0x_266 dbplyr R to SQL.r
+++ 0x_266 dbplyr R to SQL.r
@@ -1,1 +1,23 @@
-0x_267 dbplyr R to SQL 
+# dbplyr
+
+# note: d "b" plyr instead of dplyr
+
+# dbplyr is the database backend for dplyr. 
+# It allows you to use remote database tables as if 
+# they are in-memory data frames by 
+# automatically converting dplyr code into SQL.
+
+# install.packages("tidyverse")
+# Alternatively, install just dbplyr:
+# install.packages("dbplyr")
+
+library(dplyr)
+
+# library(dbplyr) isn't needed. dplyr autoloads dbplyr when
+# using sql tables
+
+library(RSQLite)
+sqlcon <- dbConnect(RSQLite::SQLite(), dbname="Chinook_Sqlite.sqlite")
+
+show_query()
+
