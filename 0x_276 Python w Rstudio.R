732962661ec6ea1e3c76badbc012adfc83d0611e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-01-18 05:20:10 -0800

    initial commit

--- 0x_276 Python w Rstudio.R
+++ 0x_276 Python w Rstudio.R
@@ -1,10 +1,15 @@
 # https://support.posit.co/hc/en-us/articles/1500007929061-Using-Python-with-the-RStudio-IDE
 # python with Rstudio
 
-install.packages("reticulate")
-library(reticulate)
+# install.packages("reticulate")
+library(reticulate) # this connects RStudio to Python
 
 # now you can run python scripts
 # note that a REPL is opened in console
+# look in tools::global options for python configuration
 
-myRObject <- c("This", "That", "The Other")
+# create an object in R. This can be seen by python
+myRObject <- c("This", "That", "The Other", "one more")
+
+# note the R/Python selector in environment pane
+
--- 0x_276 Python w Rstudio.py
+++ 0x_276 Python w Rstudio.py
@@ -1,3 +1,12 @@
+# RStudio switches into Python REPL when .py appears
 print("Hello Python")
 
 r["myRObject"] # accessing objects in the R session
+
+for mything in r["myRObject"]:
+  print(mything)
+
+# to return to R
+exit 
+
+# use "Source Script" in editor pane to run .py script
