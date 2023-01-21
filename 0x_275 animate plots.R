591130f95a5e727ad588a5faba28ff7b19c5fc36
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-01-21 12:44:38 -0800

    initial commit and renumber

---
+++ 0x_275 animate plots.R
@@ -0,0 +1,44 @@
+# animate your plots with anim.plots
+
+# install.packages("anim.plots")
+library(anim.plots)
+
+# simplify data
+splitChicks <- split(x = ChickWeight, f = ChickWeight$Chick)
+
+# normal plot -------
+plot(x = splitChicks[["1"]]$Time,
+     y = splitChicks[["1"]]$weight)
+
+# with animation
+anim.plot(x = splitChicks[["1"]]$Time,
+          y = splitChicks[["1"]]$weight)
+
+# "window" adds points -------
+anim.plot(
+  x = splitChicks[["1"]]$Time,
+  y = splitChicks[["1"]]$weight,
+  main = paste("Weight:", splitChicks[["1"]]$weight),
+  window = 1:t
+)
+
+# add lines
+plot(x = splitChicks[["1"]]$Time,
+     y = splitChicks[["1"]]$weight
+     )
+titleChicks <- paste("Chick #", 2:4)
+for (thisChick in 2:4) {
+  anim.lines(x = splitChicks[[thisChick]]$Time,
+             y = splitChicks[[thisChick]]$weight,
+             lty = thisChick,
+             window = 1:t)
+} 
+
+# other animations
+for (thisChick in 2:4) {
+  anim.arrowplot(x0 = splitChicks[[thisChick]]$Time,
+             y0 = splitChicks[[thisChick]]$weight,
+             window = 1:t)
+} 
+
+# be sure to look at documentation for MORE plot types and parameters
