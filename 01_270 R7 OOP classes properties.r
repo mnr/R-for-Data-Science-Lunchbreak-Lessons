7c10f84213bf177c077d35fd05e1ce8c0af3165a
Mark Niemann-Ross <mark@niemannross.com>
2023-01-24 10:15:16 -0800

    split classes and properties

--- 01_270 R7 OOP classes properties.r
+++ 01_270 R7 OOP classes properties.r
@@ -1,16 +1,12 @@
 # R7 OOP structures
 # https://github.com/RConsortium/OOP-WG
 # https://rconsortium.github.io/OOP-WG/articles/R7.html
-# classes and properties
-
+# properties
 
 # install.packages("remotes")
 # remotes::install_github("rconsortium/OOP-WG")
 library(R7)
 
-# demonstration of:
-# base_classes
-# new_property
 source("01_269 class planet.R")
 
 # how it works
@@ -29,25 +25,3 @@ Mars@timeNow
 # set a getter/setter
 Mars@timeNow <- Sys.time()
 Mars
-
-# Create a class from a parent class
-baseClass <- new_class(
-  name = "baseClass",
-  properties = list(bcName = class_character,
-                    aValue = class_numeric),
-  abstract = TRUE
-)
-
-# since this is an abstract, it can't be used to create an object
-mybc <- baseClass(bcName = "myname", aValue = 15) 
-
-upClass <- new_class(
-  name = "upClass",
-  parent = baseClass,
-  properties = list(
-    anotherValue = class_numeric
-    )
-)
-
-newUpClass <- upClass(anotherValue = 3, bcName = "bob", aValue = 15)
-newUpClass
---
+++ 0x_272 R7 OOP base class.r
@@ -0,0 +1,30 @@
+# R7 OOP structures
+# https://github.com/RConsortium/OOP-WG
+# https://rconsortium.github.io/OOP-WG/articles/R7.html
+# base classes
+
+# install.packages("remotes")
+# remotes::install_github("rconsortium/OOP-WG")
+library(R7)
+
+# Create a class from a parent class
+baseClass <- new_class(
+  name = "baseClass",
+  properties = list(bcName = class_character,
+                    aValue = class_numeric),
+  abstract = TRUE
+)
+
+# since this is an abstract, it can't be used to create an object
+mybc <- baseClass(bcName = "myname", aValue = 15) 
+
+upClass <- new_class(
+  name = "upClass",
+  parent = baseClass,
+  properties = list(
+    anotherValue = class_numeric
+    )
+)
+
+newUpClass <- upClass(anotherValue = 3, bcName = "bob", aValue = 15)
+newUpClass
