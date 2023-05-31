41074d89c7ba703ab0860b65be832868471b17bf
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-05-31 09:27:58 -0500

    initial commit

--- 0x_283 rtoot.R
+++ 0x_283 rtoot.R
@@ -3,14 +3,15 @@
 # install.packages("rtoot")
 library(rtoot)
 
-
 # authenticate to a mastodon server
 auth_setup(instance = "mastodon.social", type = "public")
 
 # get list of federated instances
-# first - get a token from https://instances.social/api/token
+
+myMastodonSocialToken <- "get a token from https://instances.social/api/token"
+
 fedis <- get_fedi_instances(n = 130,
-                            token = "obtained_from_https://instances.social/api/token")
+                            token = myMastodonSocialToken)
 
 # looking to do some textmining? 
 publictoots <- get_timeline_public(limit = 13,
