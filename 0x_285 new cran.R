3d4a63ed8387cb44c6dd3c256ccf2a64e308d1ba
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-05-31 10:41:45 -0500

    init commit

---
+++ 0x_285 new cran.R
@@ -0,0 +1,11 @@
+# nes r cran task views
+
+https: /  / cran.r - project.org / web / views /
+  
+  CRAN Task Views have been available on the Comprehensive R Archive Network since 2005. They provide guidance about which CRAN packages are relevant for tasks related to a certain topic, and can also facilitate automatic installation of all corresponding packages. Motivated by challenges from the growth of CRAN and the R community as a whole since 2005, all of the task views infrastructure and workflows were rethought and relaunched in 2021 /
+  22 in order to facilitate maintenance, and to foster deeper interactions with the R community. The redesign encompasses the establishment of a group of CRAN Task View Editors, moving all task view sources to dedicated GitHub repositories, adopting well -
+  documented workflows with a code of conduct, and leveraging R / Markdown files (rather than XML) for the content of the task views.
+
+https: /  / arxiv.org / pdf / 2305.17573.pdf
+
+https: /  / github.com / cran - task - views / ctv / 
